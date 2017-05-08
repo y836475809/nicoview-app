4f754f6456c8d68ee54af7fae179a5aa27ab7e1a
y836475809 <tttdaytttday@gmail.com>
2017-05-09 03:22:07 +0900

    anime

---
+++ comment.js
@@ -0,0 +1,101 @@
+
+// window.onload = () => {
+//     $('#add-btn').on("click", () => {
+//         var ele = document.createElement("div")
+//         ele.innerHTML = "message"
+//         ele.className = "comment"
+//         ele.style.left = 300 + "px"
+//         ele.style.top = 50 + "px"
+//         console.log('aaaaaaaaaaaaaaa')
+//         // $('#container').appendChild(ele)
+//         document.getElementById("container").appendChild(ele);
+//     });
+// }
+
+class comment_anime{
+    constructor(parent_id){
+        this.parent_id = parent_id
+    }
+
+    delete_elm(){
+
+    }
+    
+    create_flow(text, width, duration){
+        
+    }
+
+    create_fix(){
+
+    }
+}
+
+let create_comment_elm = (parent_id,  text, width, duration) => {
+    let ele = document.createElement("div")
+    ele.innerHTML = text
+    ele.className = "comment"
+    // ele.setAttribute("data-x", -left)
+    // ele.setAttribute("data-duration", 5000)
+    // ele.style.left = left + "px"
+    // ele.style.top = top + "px"
+
+    document.getElementById(parent_id).appendChild(ele);
+    let rect = ele.getBoundingClientRect()
+    let left = width
+    let len = width + rect.width
+    let sp = len / duration
+
+    ele.style.left = left + "px"
+    // ele.style.top = top + "px"
+    ele.setAttribute("data-x", -len)
+    ele.setAttribute("data-duration", duration)
+
+    return {ele:ele, sp:sp}
+    // document.getElementById("container").appendChild(ele);
+}
+
+let create_fix_comment = (parent_id,  text, width, duration) => {
+    let ele = document.createElement("div")
+    ele.innerHTML = text
+    ele.className = "fix_comment"
+    // ele.setAttribute("data-x", -left)
+    // ele.setAttribute("data-duration", 5000)
+    // ele.style.left = left + "px"
+    // ele.style.top = top + "px"
+
+    document.getElementById(parent_id).appendChild(ele);
+    let rect = ele.getBoundingClientRect()
+    let left = width
+    let len = width + rect.width
+    let sp = len / duration
+
+    ele.style.left = left + "px"
+    // ele.style.top = top + "px"
+    ele.setAttribute("data-x", -len)
+    ele.setAttribute("data-duration", duration)
+
+    return {ele:ele, sp:sp}
+    // document.getElementById("container").appendChild(ele);
+}
+
+// let set_comment_param = (c_param, top, width, duration) => {
+//     // document.getElementById(id).appendChild(ele);
+//     // let rect = ele.getBoundingClientRect()
+//     let ele = c_param.ele
+//     let rect = c_param.rect
+//     let len = width + rect.width
+//     // let sp = len / duration
+
+//     ele.style.left = len + "px"
+//     ele.style.top = top + "px"
+//     ele.setAttribute("data-x", -len)
+//     ele.setAttribute("data-duration", duration)
+
+//     return { speed: sp }
+// }
+
+let set_comment_param = (text, left, top) => {
+
+}
+
+// module.exports = create_comment_elm
--- player.js
+++ player.js
@@ -2,9 +2,8 @@
 var video_size = {}
 
 var getContentSize = () => {
-    let w = document.documentElement.clientWidth - 16
-    let h = document.documentElement.clientHeight - 16
-
+    let w = window.innerWidth - 16
+    let h = window.innerHeight - 16
     return { width: w, height: h }
 }
 
@@ -14,7 +13,7 @@ var getVideoSize = () => {
 
     let v_h = w_h - ctr_h
     let v_w = video_size.width / video_size.height * v_h
-
+    // console.log("getVideoSize video_size=", video_size)
     let c_size = getContentSize()
     if (v_w < c_size.width) {
         return { width: v_w, height: v_h }
@@ -25,23 +24,116 @@ var getVideoSize = () => {
     }
 }
 
-var setVideoContainerSize = () => {
-    let h = getContentSize().height
-    let player_ctr_h = $('#player-ctr').height()
-    let con = document.getElementById("container")
 
-    con.style.height = (h - player_ctr_h) + "px"
+var setPlayerContainerSize = () => {
+    let h = getContentSize().height
+    let w = getContentSize().width
+    let player_ctr_h = $('#player-ctr').height()
+
+    let v_size = getVideoSize()
+    // console.log("v_size=", v_size)
+
+    let player = $('#player').get(0)
+    // play.style.left = (h - player_ctr_h)  + "px"
+    let play_top = (h - player_ctr_h) / 2 - (v_size.height) / 2
+    let play_left = w / 2 - (v_size.width) / 2
+    player.style.top = play_top + "px"
+    player.style.left = play_left + "px"
 }
 
+var setVideoContainerSize = () => {
+    let h = getContentSize().height
+    let w = getContentSize().width
+    let player_ctr_h = $('#player-ctr').height()
+
+    let con = document.getElementById("container")
+    con.style.height = (h - player_ctr_h) + "px"
+    con.style.clip = "rect(0px " + w + "px " + (h - player_ctr_h) + "px 0px)"
+ 
+    let ctr = $('#player-ctr').get(0)
+    ctr.style.top = (h+8 - player_ctr_h) + "px"
+}
+
+
 window.onload = () => {
+
     setVideoContainerSize()
 
     $('#play-btn').on("click", () => {
         let video = $('#player').get(0)
-        // video.src = "mov/test.mp4"
-        video.src = "mov/oc.mp4"
+        video.src = "mov/test.mp4"
+        // video.src = "mov/oc.mp4"
         video.type = "video/mp4"
         video.load();
+    });
+
+    $('#add-btn').on("click", () => {
+        const top = 50
+        const duration = 5000
+        let w = getContentSize().width
+        let params = create_comment_elm("container","message", w, duration)
+        let sp = params.sp
+        let ele = params.ele
+        // document.getElementById("container").appendChild(ele);
+        // let rect = ele.getBoundingClientRect()
+        ele.style.top = top + "px"
+        // const duration = 5000
+        // let param = set_comment(ele, "container", top, w, duration)
+
+        // console.log(rect)
+        let params2 = create_comment_elm("container","message", w, duration)
+        let ele2 = params2.ele
+        // document.getElementById("container").appendChild(ele);
+        // let rect = ele.getBoundingClientRect()
+        ele2.style.top = top*2 + "px"
+        // const duration = 5000
+        // let param = set_comment(ele, "container", top, w, duration)
+        
+        var comment_anime = anime({
+            targets: '.comment',
+            translateX: function (el) {
+                return el.getAttribute('data-x')
+            },
+            // translateY: function (el, i) {
+            //     // return (30 * i);
+            //     return 50;
+            // },
+            duration: function (target) {
+                return target.getAttribute('data-duration')
+            },
+            delay: function (target, index) {
+                // console.log(index)
+                return index * 1000
+            },
+            easing: 'linear',
+            loop: false,
+            autoplay: false
+        });
+
+        let pp = create_fix_comment("container", "1fixed_message", w-250, 1000)
+        let f_ele = pp.ele
+        f_ele.style.display = 'none'
+        // create_fix_comment("container", "2fixed_message", w, 1000)
+        var myAnimation = anime({
+        targets: '.fix_comment',
+        translateX: 0,
+        delay: 2000,
+        duration: 5000,
+        begin: function(anim) {
+            let ele = anim.animatables[0].target
+            ele.style.display = 'inline'
+            // console.log(index);
+            // console.log(anim);
+            console.log(anim.began); // true after 1000ms
+        },
+        complete: function(anim) {
+            let ele = anim.animatables[0].target
+            ele.style.display = 'none'
+            console.log("anim.completed=", anim.completed);
+        }        
+        });
+
+        comment_anime.play()
     });
 
     $('#player').on('loadedmetadata', function (event) {
@@ -50,6 +142,8 @@ window.onload = () => {
 
         video_size = { width: w, height: h }
 
+        setPlayerContainerSize()
+
         const v_size = getVideoSize()
         $('#player').width(v_size.width)
         $('#player').height(v_size.height)
@@ -57,6 +151,7 @@ window.onload = () => {
 
     window.addEventListener('resizeend', function () {
         setVideoContainerSize()
+        setPlayerContainerSize()
 
         const v_size = getVideoSize()
         $('#player').width(v_size.width)

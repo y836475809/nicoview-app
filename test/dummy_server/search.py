b9cd4f0a4258a939268d3927a55ed4170d33828e
test <test@example.com>
2018-09-14 17:30:10 +0900

    add test json data

--- test/dummy_server/search.py
+++ test/dummy_server/search.py
@@ -43,12 +43,35 @@
 from bottle import Bottle, run, route, request, response
 import json
 
+json_data = None
 app = Bottle()
 
 
 @route('/api.search.nicovideo.jp/api/v2/<service:re:[a-z]+>/contents/search', method='GET')
 def serach(service):
     response.headers['Content-Type'] = 'application/json'
+    key_word = request.query.q
+    targets = request.query.targets.split(",")
+    fields = request.query.fields.split(",")
+    sort = request.query._sort
+    offset = request.query._offset
+    limit = request.query._limit
+    context = request.query._context
+    non = request.query.non # ''
+
+    # fp = open("./data/search.json", encoding="utf-8")
+    # json_obj = json.load(fp)
+    # datas = json_obj["data"]
+    datas = json_data
+
+    results = []
+    for target in targets:
+        results.append(list(filter(lambda data: key_word in data[target], datas)))
+
+    result = flatten(results)
+    unique_result = unique(result)
+
+    # result = {k: v for k, v in json_obj.items() if 'オリジナル' in k}
     return getJson()
 
 
@@ -61,9 +84,25 @@ def counter():
 
 
 def getJson():
-    obj = {'TV': 100, 'camera': 200, 'radio': 300}
-    return json.dumps(obj, indent=2)
+    # obj = {'TV': 100, 'camera': 200, 'radio': 300}
+    # return json.dumps(obj, indent=2)
+    s = ""
+    with open("./tmp/search0-10.json", encoding="utf-8") as f:
+        s = f.read()
+    return s
+
+
+def flatten(nested_list):
+    return [e for inner_list in nested_list for e in inner_list]
+
+
+def unique(dic_list):
+    return [dict(t) for t in set([tuple(d.items()) for d in dic_list])]
 
 
 if __name__ == "__main__":
+    fp = open("./data/search.json", encoding="utf-8")
+    json_data = json.load(fp)
+    fp.close()
+
     run(host='localhost', port=8083, debug=True)

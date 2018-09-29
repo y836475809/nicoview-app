9bfeeb8776ac1f2360275ed1c1f7bdfda937d329
y836475809 <tttdaytttday@gmail.com>
2018-09-30 02:59:17 +0900

    use jest

---
+++ __tests__/reader.test.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const reader = require("../app/js/reader")
+
+const dir = `${__dirname}/data`;
+
+test("read comment", function () {
+    const xml = fs.readFileSync(`${dir}/sample.xml`, "utf-8");
+    const obj = reader.comment(xml);
+    expect(obj).toEqual(
+        [
+            {no:1,  vpos:400, date:0, user_id:"AAA", mail:"naka medium 184", text:"AAAテスト"},
+            {no:2,  vpos:300, date:1, user_id:"BBB", mail:"184", text:"BBBあ"},
+            {no:3,  vpos:1500, date:2, user_id:"CCC", mail:"184", text:"CCCテスト"},
+            {no:4,  vpos:4000, date:3, user_id:"CCC", mail:"184", text:"CCCテストテスト"},
+            {no:5,  vpos:100, date:4, user_id:"DDD", mail:"184 device:3DS", text:"DDDテスト"},
+            {no:6,  vpos:4500, date:5, user_id:"EEE", mail:"184", text:"EEEテスト"}
+        ]);
+});
+
+test("read thumb info", function () {
+    const xml = fs.readFileSync(`${dir}/sample[ThumbInfo].xml`, "utf-8");
+    const obj = reader.thumb_info(xml);
+
+    expect(obj.video_id).toEqual("sm1000");
+    expect(obj.title).toEqual("sample.mp4");
+    expect(obj.description).toEqual("投稿コメントサンプル。他sm2000リストmylist/3000");
+    expect(obj.first_retrieve).toEqual("2000-01-01T01:02:03+09:00");
+    expect(obj.length).toEqual("00:45");
+    expect(obj.movie_type).toEqual("mp4");
+    expect(obj.size_high).toBe(1000);
+    expect(obj.size_low).toBe(500);
+    expect(obj.view_counter).toBe(100);
+    expect(obj.comment_num).toBe(10);
+    expect(obj.mylist_counter).toBe(5);
+    expect(obj.last_res_body).toEqual("最新コメント1 最新コメント2");
+    expect(obj.tags).toEqual([
+        {tag:"タグ1", lock:"1"},
+        {tag:"タグ2", lock:""},
+        {tag:"タグ3", lock:""}
+    ]);
+    expect(obj.user_id).toEqual("00000");
+    expect(obj.user_nickname).toEqual("ニックネーム");
+    expect(obj.user_icon_url).toEqual("https://secure-dcdn.cdn.nimg.jp/nicoaccount/usericon/defaults/blank_s.jpg");
+});
---
+++ __tests__/sqlite.test.js
@@ -0,0 +1,60 @@
+var SQLiteDB = require("../app/js/sqlite_db");
+
+const db_file_path = `${__dirname}/data/sample.db`;
+
+test("sqlite db dirpath", function () {
+    let db = new SQLiteDB();
+    db.init(db_file_path);
+    db.read_dirpath();
+    const dirpath_map = db.get_dirpath();
+    expect(dirpath_map).toEqual(
+        new Map()
+            .set(1, "file:///C:/data/サンプル")
+            .set(2, "file:///C:/data")
+    );
+});
+
+test("sqlite db tag", function () {
+    let db = new SQLiteDB();
+    db.init(db_file_path);
+    db.read_tag_string();
+    db.read_tag();
+    const tag_map = db.tag_map;
+    expect(tag_map).toEqual(
+        new Map()
+            .set(1, ["タグ1", "タグ2"])
+            .set(2, ["タグ1", "タグ2", "タグ3"])
+            .set(3, ["タグ4"])
+            .set(4, ["タグ5"])
+            .set(5, ["タグ3", "タグ4", "タグ5"])
+    );
+});
+
+test("sqlite db video", function () {
+    let db = new SQLiteDB();
+    db.init(db_file_path);
+    db.read();
+    const video_map = db.get_video();
+
+    expect(video_map.has("sm1")).toBeTruthy();
+    expect(video_map.has("sm2")).toBeTruthy();
+    expect(video_map.has("sm3")).toBeTruthy();
+    expect(video_map.has("sm4")).toBeTruthy();
+    expect(video_map.has("sm5")).toBeTruthy();
+
+    const sm1 = video_map.get("sm1");
+    const sm2 = video_map.get("sm2");
+    const sm3 = video_map.get("sm3");
+    const sm4 = video_map.get("sm4");
+    const sm5 = video_map.get("sm5");
+
+    expect(sm1.video_name).toEqual("サンプル1");
+    expect(sm1.video_filename).toEqual("サンプル1 - [sm1].mp4");
+    expect(sm1.video_type).toEqual("video/mp4");
+
+    expect(sm1.tags).toEqual(["タグ1", "タグ2"]);
+    expect(sm2.tags).toEqual(["タグ1", "タグ2", "タグ3"]);
+    expect(sm3.tags).toEqual(["タグ4"]);
+    expect(sm4.tags).toEqual(["タグ5"]);
+    expect(sm5.tags).toEqual(["タグ3", "タグ4", "タグ5"]);
+});

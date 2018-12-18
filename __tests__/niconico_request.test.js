372ffb349d635a6a300afe22ef6b94d4c2e5270b
y836475809 <tttdaytttday@gmail.com>
2018-12-19 01:34:23 +0900

    wip: test request

--- __tests__/niconico_request.test.js
+++ __tests__/niconico_request.test.js
@@ -1,27 +1,50 @@
 const nock = require("nock");
-const rp = require("request-promise");
-const request = require("request");
-
-const url = "https://test.com";
-const path = "/api/v2/items";
+const { NicoNico, NicoCommnet } = require("../test/test-client");
 
 
-test("http req", () => {
-    nock(url).get(path).reply(200, {
-        "response": "Hello, World!"
+// test("http req", async () => {
+//     // jest.setTimeout(20000);
+//     nock("http://www.nicovideo.jp")
+//         .get("/watch/sm19961784")
+//         // .delay(12000)
+//         .replyWithFile(200, `${__dirname}/data/sm19961784.html`, { "Content-Type": "html/text" });
+
+//     const niconico = new NicoNico();
+//     const data = await niconico.watch("sm19961784");
+//     console.log(data);
+//     // niconico.watch("sm19961784").then(b=>{
+//     //     console.log(b);
+//     // });
+// });
+
+
+test("http req cancel", async () => {
+    // jest.setTimeout(20000);
+    nock("http://www.nicovideo.jp")
+        .get("/watch/sm19961784")
+        .delay(2000)
+        .replyWithFile(200, `${__dirname}/data/sm19961784.html`, { "Content-Type": "html/text" });
+
+    const niconico = new NicoNico();
+    niconico.watch("sm19961784").then(b=>{
+        console.log("done");
     });
-
-    const cookieJar = request.jar();
-    const options = {
-        uri: `${url}${path}`,
-        method: "GET",
-        jar: cookieJar,
-        timeout: 10 * 1000
-    };
-    const req = rp(options);
-    req.then((body) => {
-        console.log("body=", body);
-    }).catch((error)=>{
-        throw error;
+    niconico.watch("sm19961784").then(b=>{
+        return niconico.postDmcSession()
     });
+    // console.log("cancel");
+    // p.abort();
+    // setTimeout(()=>{
+    //     console.log("cancel");
+    //     niconico.cancel();
+    // }, 500);
+    // try {
+    //     const data = await niconico.watch("sm19961784");
+    //     console.log(data);    
+    // } catch (error) {
+    //     console.log(error);    
+    // }
+
+
+
 });

7452c4c8757010c5740ab2f32a7e50705591a2ab
y836475809 <tttdaytttday@gmail.com>
2018-12-20 01:52:03 +0900

    wip: client test

--- __tests__/niconico_request.test.js
+++ __tests__/niconico_request.test.js
@@ -1,120 +1,73 @@
 const nock = require("nock");
-const { NicoNico, NicoCommnet } = require("../test/test-client");
-var request = require('request');
+const request = require("request");
 const express = require("express");
 const fs = require("fs");
-// test("http req", async () => {
-//     // jest.setTimeout(20000);
-//     nock("http://www.nicovideo.jp")
-//         .get("/watch/sm19961784")
-//         // .delay(12000)
-//         .replyWithFile(200, `${__dirname}/data/sm19961784.html`, { "Content-Type": "html/text" });
-
-//     const niconico = new NicoNico();
-//     const data = await niconico.watch("sm19961784");
-//     console.log(data);
-//     // niconico.watch("sm19961784").then(b=>{
-//     //     console.log(b);
-//     // });
-// });
+const { NicoNico, NicoCommnet } = require("../test/test-client");
 
 
-test("http req cancel", () => {
-    jest.setTimeout(20000);
-    
-    const app = express();
-    // app.use(express.static('dest'));
+describe("http", () => {
     const port = 8084;
-    // app.listen(port, ()=> {
-    //     console.log("Expressサーバー起動");
-    // });
-    // app.get("/watch/sm19961784", (req, res)=>{
-    //     // res.send('hello world');
+    let server;
+
+    beforeAll(() => {
+        console.log("beforeAll");
+
+        const app = express();
+        app.get("/watch/:videoid", (req, res) => {
+            const video_id = req.params.videoid;
+            const file = fs.readFileSync(`${__dirname}/data/${video_id}.html`, "utf-8");
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(file);
+            res.end();
+        });
+        server = app.listen(port, () => {
+            console.log("Expressサーバー起動");
+        });
+    });
+
+    afterAll(() => {
+        console.log("afterAll");
+        server.close();
+    });
+
+    test("http req cancel", (done) => {
+        jest.setTimeout(20000);
+
+        nock.disableNetConnect();
+        nock.enableNetConnect("localhost");
         
-    //     setTimeout(()=>{
-    //         const file = fs.readFileSync(`${__dirname}/data/sm19961784.html`, "utf-8");
-    //         res.writeHead(200, {"Content-Type" : "text/html"});
-    //         res.write(file);
-    //         res.end();
-    //     }, 5*1000);
-
-    // });
-    // const server = app.listen(port, ()=> {
-    //     console.log("Expressサーバー起動");
-    // });
-    // app.listen(port);
-
-
-    
-    // // nock.cleanAll();
-    // nock.disableNetConnect();
-    // nock.enableNetConnect('localhost:8084');
-    // nock.disableNetConnect();
-    // Allow localhost connections so we can test local routes and mock servers.
-    // nock.enableNetConnect('127.0.0.1');
-    // nock.enableNetConnect('127.0.0.1');
-    nock("http://127.0.0.1:8084")
-        .get("/watch/sm19961784")
-        .delay(3000)
-        .reply(200, "ok");
+        nock("http://localhost:8084")
+            .get("/watch/sm19961784")
+            .delay(1000);
+        // .reply(200, "ok");
+        // .reply(200, (uri, requestBody) => {
+        //     console.log(requestBody);
+        //     return requestBody;
+        // });
         // .replyWithFile(200, `${__dirname}/data/sm19961784.html`, { "Content-Type": "html/text" });
 
-    // const niconico = new NicoNico("http://localhost:8084");
-    // niconico.watch("sm19961784").then(b=>{
-    //     console.log("done: ", b);
-    // }).catch((error)=>{
-    //     // console.log("error");
-    //     console.log("error:", error);
-    // });
-    function doRequest(options) {
-        return new Promise((resolve, reject) => {
-            request(options, (error, res, body) => {
-                if (!error && res.statusCode == 200) {
-                    resolve(body);
-                } else {
-                    reject(error);
-                }
-            });
+        const niconico = new NicoNico(`http://localhost:${port}`);
+        niconico.watch("sm19961784").then(b=>{
+            console.log("done: ", b);
+            done();
+        }).catch((error)=>{
+            // console.log("error");
+            console.log("error:", error);
+            done();
         });
-    }
 
-    const options = {
-        url: `http://127.0.0.1:${port}/watch/sm19961784`,
-        method: 'GET',
-        timeout: 20*1000
-    };
-    // const hoge = async () =>{
-    //     const res = await doRequest(options);
-    //     console.log("rr=", res);
-    // };
-    // hoge();
-    // const rr = await doRequest(options);
-    // console.log("rr=", rr);
-    // // var req = http.get('http://www.dummy.nicovideo.jp/watch/sm19961784');
-    // request(`http://localhost:${port}/watch/sm19961784`, (err, response, body) => {
-    request(options, (err, response, body) => {
-        
-        console.log(body);
-        console.log("body");
-        // server.close();
-    }); 
-    // niconico.cancel();
-    // niconico.watch("sm19961784").then(b=>{
-    //     return niconico.postDmcSession()
-    // });
-    // console.log("cancel");
-    // p.abort();
-    // setTimeout(()=>{
-    //     console.log("cancel");
-    //     niconico.cancel();
-    // }, 500);
-    // try {
-    //     const data = await niconico.watch("sm19961784");
-    //     console.log(data);    
-    // } catch (error) {
-    //     console.log(error);    
-    // }
+        // const options = {
+        //     url: `http://localhost:${port}/watch/sm19961784`,
+        //     method: "GET",
+        //     timeout: 20 * 1000
+        // };
+        // request(options, (err, response, body) => {
+        //     console.log(body);
+        //     console.log("body");
+        //     expect(body).toBe("ok");
 
+        //     done();
+        // });
 
-
+    });
 });

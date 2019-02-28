e8890d84d12e8d546da7fc74aae5bef89310fb70
test <test@example.com>
2019-02-28 17:54:54 +0900

    wip: nico search

--- app/js/niconico-search.js
+++ app/js/niconico-search.js
@@ -1,15 +1,50 @@
 const querystring = require("querystring");
 
-const q = {
-    service: "video",
-    q: "word",
-    targets: ["title", "description", "tags"].join(","),
-    fields:  [
-        "contentId","title","description","tags",
-        "viewCounter","commentCounter","startTime", 
-        "thumbnailUrl","lengthSeconds"].join(","),
-    _sort: this._sort,
-    _offset: this._size * (this._page - 1),
-    _limit: this._size,
-    _context: "test"
-};
+class NicoSearch {
+    constructor(){
+        this._service = "video";
+        this._limit = 32;
+        this._fields = [
+            "contentId","title","description","tags",
+            "viewCounter","commentCounter","startTime", 
+            "thumbnailUrl","lengthSeconds"];
+    }
+
+    _query(){
+        return {
+            service: this._service,
+            q: this._word,
+            targets: this.targets.join(","),
+            fields: this._fields.join(","),
+            _sort: this._sort,
+            _offset: this._limit * (this._page - 1),
+            _limit: this._limit,
+            _context: "test"
+        };
+    }
+
+    query(){
+        return querystring.stringify(this._query());
+    }
+
+    keyword(word){
+        this._word = word;
+        this.targets = ["title", "description", "tags"];
+    }
+
+    tag(word){
+        this._word = word;
+        this.targets = ["tagsExact"];
+    }
+
+    page(num_page){
+        this._page = num_page;
+    }
+    
+    sortKind(kind){
+        this._sort_kind = kind;
+    }
+    sortOder(order){
+        this._sort_order = order;
+    }
+}

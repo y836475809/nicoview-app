732f22c64cd0736d94cc92778b8ff59e890209f3
y836475809 <tttdaytttday@gmail.com>
2019-03-04 03:18:20 +0900

    wip: niconico search
    add mock server

---
+++ test/mock_server/index.js
@@ -0,0 +1,30 @@
+
+module.exports = () = {
+  const total_count = 1000;
+  result = {
+    data: []
+  };
+  for(let i = 0; i < 10; i++) {
+    const search_result = { 
+      meta: {
+        totalCount: total_count
+      },
+      data: [] 
+    };
+    for(let j = 0; j < 32; j++) {
+      search_result.data.push(
+        { 
+          thumbnailUrl: `http://localhost:3001/img${Math.floor(Math.random()*6)}.jpeg`,
+          contentId: `sm${32*i + j}`, 
+          title: `user${32*i +j}`,
+          tags: "tag1, tsg2 tag3",
+          viewCounter: Math.floor(Math.random()*100),
+          commentCounter: Math.floor(Math.random()*100),
+          lengthSeconds: Math.floor(Math.random()*300),
+          startTime: "2010-10-01T20:28:02+09:00"
+        })
+    }
+    result.data.push(search_result);
+  }
+  return result;
+}

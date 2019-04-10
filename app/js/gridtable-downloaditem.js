4418e356eb4baec97816c3a884dfe88492a98316
y836475809 <tttdaytttday@gmail.com>
2019-04-11 01:55:25 +0900

    wip: downloader
    arrange download-page(wip)

---
+++ app/js/gridtable-downloaditem.js
@@ -0,0 +1,261 @@
+require("slickgrid/lib/jquery.event.drag-2.3.0");
+require("slickgrid/lib/jquery.event.drop-2.3.0");
+require("slickgrid/plugins/slick.rowmovemanager");
+const { GridTable } = require("./gridtable");
+const { SettingStore } = require("./setting-store");
+const { DownloadItemStore } = require("./download-item-store");
+
+class GridTableDownloadItem {
+    constructor(parent_elm, state_formatter){
+        const columns = [
+            {id: "thumb_img", name: "image", height:100, width: 130, behavior: "selectAndMove"},
+            {id: "id", name: "id", behavior: "selectAndMove"},
+            {id: "name", name: "名前", behavior: "selectAndMove"},
+            {id: "state", name: "状態", behavior: "selectAndMove", formatter:state_formatter}
+        ];
+        const options = {
+            rowHeight: 100,
+            enableCellNavigation: true,
+            enableColumnReorder: false,
+            _saveColumnWidth: true
+        };
+        this.grid_table = new GridTable("download-item-grid", columns, options);
+        this.grid_table.init(parent_elm);
+            
+        this.grid_table.dataView.onRowCountChanged.subscribe((e, args) => {
+            this.grid_table.grid.updateRowCount();
+            this.grid_table.grid.render();
+        });
+
+        this.grid_table.dataView.onRowsChanged.subscribe((e, args) => {
+            this.grid_table.grid.invalidateRows(args.rows);
+            this.grid_table.grid.render();
+        });
+
+        const moveRowsPlugin = new Slick.RowMoveManager({
+            cancelEditOnDrag: true
+        });
+
+        moveRowsPlugin.onBeforeMoveRows.subscribe((e, data) => {
+            for (let i = 0; i < data.rows.length; i++) {
+                if (data.rows[i] == data.insertBefore || data.rows[i] == data.insertBefore - 1) {
+                    e.stopPropagation();
+                    return false;
+                }
+            }
+            return true;
+        });
+
+        moveRowsPlugin.onMoveRows.subscribe((e, args) => {
+            let data = this.grid_table.dataView.getItems();
+            let extractedRows = [], left, right;
+            const rows = args.rows;
+            const insertBefore = args.insertBefore;
+            left = data.slice(0, insertBefore);
+            right = data.slice(insertBefore, data.length);
+
+            rows.sort(function(a,b) { return a-b; });
+
+            for (let i = 0; i < rows.length; i++) {
+                extractedRows.push(data[rows[i]]);
+            }
+
+            rows.reverse();
+
+            for (let i = 0; i < rows.length; i++) {
+                const row = rows[i];
+                if (row < insertBefore) {
+                    left.splice(row, 1);
+                } else {
+                    right.splice(row - insertBefore, 1);
+                }
+            }
+
+            data = left.concat(extractedRows.concat(right));
+
+            const selectedRows = [];
+            for (let i = 0; i < rows.length; i++){
+                selectedRows.push(left.length + i);
+            }
+
+            this.grid_table.grid.resetActiveCell();
+            this.grid_table.dataView.beginUpdate();
+            this.grid_table.dataView.setItems(data);
+            this.grid_table.dataView.endUpdate();
+
+            this.grid_table.grid.setSelectedRows(selectedRows);
+
+            this.save();
+        });
+
+        this.grid_table.grid.registerPlugin(moveRowsPlugin); 
+    }
+
+    init(on_context_menu, on_dbl_click){
+        this.store = new DownloadItemStore(SettingStore.getSystemFile("download.json"));
+
+        this.grid_table.onContextMenu((e)=>{
+            on_context_menu(e);
+        });
+
+        this.grid_table.onDblClick((e, data)=>{
+            on_dbl_click(e, data);
+        });
+
+        this.store.load(); 
+        this.grid_table.setData(this.store.getItems());
+        this.grid_table.dataView.setFilter((item)=>{
+            return item.visible===true;
+        });
+    }
+
+    resizeFitContainer(container){
+        this.grid_table.resizeFitContainer(container);
+    }
+
+    save(){
+        this.store.setItems(this.grid_table.dataView.getItems()); 
+        this.store.save(); 
+    }
+
+    hasItem(id){
+        return this.grid_table.dataView.getRowById(id) !== undefined;
+    }
+
+    getItemByIdx(index){
+        return this.grid_table.dataView.getItemByIdx(index);
+    }
+
+    getItemIDSet(){
+        const items = this.grid_table.dataView.getItems();
+        const id_set = new Set();
+        items.forEach(value => {
+            if(value.visible){
+                id_set.add(value.id);
+            }
+        });
+        return id_set;    
+    }
+
+    addItems(items, state){
+        items.forEach(item => {
+            const dv_items = this.grid_table.dataView.getItems();
+            const fd_item = dv_items.find(value=>{
+                return value.id == item.id;
+            });
+            if(fd_item===undefined){
+                item["visible"] = true;
+                item["state"] = state;
+                item["progress"] = "";
+                this.grid_table.dataView.addItem(item);
+            }else{
+                this.grid_table.dataView.deleteItem(fd_item.id);
+                fd_item["visible"] = true;
+                fd_item["state"] = state;
+                fd_item["progress"] = "";
+                this.grid_table.dataView.addItem(fd_item);
+            }
+        });
+
+        this.grid_table.dataView.refresh();
+        this.save();
+    }
+
+    deleteItems(video_ids){
+        video_ids.forEach(video_id => {
+            if(this.hasItem(video_id)){
+                const item = this.grid_table.dataView.getItemById(video_id);
+                item.visible = false;
+            }
+        });
+        this.grid_table.dataView.refresh();
+        this.save();
+    }
+
+    deleteSelectedItems(){
+        const items = this.grid_table.getSelectedDatas();
+        items.forEach(value => {
+            const item = this.grid_table.dataView.getItemById(value.id);
+            item.visible = false;
+        });
+        this.grid_table.dataView.refresh();
+        this.grid_table.grid.setSelectedRows([]);
+        this.grid_table.grid.resetActiveCell();
+        this.save();
+
+        const deleted_ids = items.map(value => {
+            return value.id;
+        });
+        return deleted_ids;
+    }
+
+    clearItems(target_state){
+        const items = this.grid_table.dataView.getItems();
+        items.forEach(item => {
+            if(item.state === target_state){
+                this.grid_table.dataView.deleteItem(item.id);
+            }
+        });
+        this.save();
+    }
+
+    updateItem(video_id, progress, state){
+        const row_index = this.grid_table.dataView.getRowById(video_id);
+        if(row_index===undefined){
+            return;
+        }
+        const column_index = this.grid_table.grid.getColumnIndex("state");     
+        const item = this.grid_table.dataView.getItemById(video_id);
+        if(item.visible===false){
+            return;
+        }
+        item.progress = progress;
+        item.state = state;
+        this.grid_table.grid.updateCell(row_index, column_index);
+    }
+
+    canDownload(video_id, target_state){
+        const items = this.grid_table.dataView.getItems();
+        const f = items.find(value=>{
+            return value.id == video_id;
+        });
+        if(f===undefined){
+            return false;
+        }
+        if(f.visible===false){
+            return false;
+        }
+        return f.state === target_state;
+    }
+
+    getNextVideoID(video_id){
+        const items = this.grid_table.dataView.getItems();
+        let index = items.findIndex(value=>{
+            return value.id == video_id;
+        });
+        if(index===-1){
+            throw new Error(`not find ${video_id}`);
+        }
+        index++;
+        if(index >= items.length){
+            return undefined;
+        }
+
+        let next_item = items[index];
+        if(next_item.visible===false){
+            while(index < items.length){  
+                next_item = items[index];
+                if(next_item.visible===true){
+                    return next_item.id;
+                }
+                index++;
+            }
+            return undefined;
+        }
+        return next_item.id;
+    }
+}
+
+module.exports = {
+    GridTableDownloadItem: GridTableDownloadItem
+};
--- app/tags/download-page.tag
+++ app/tags/download-page.tag
@@ -27,7 +27,9 @@
         <button class="download-button" onclick={onclickStopDownload}>stop</button>
         <button class="download-button clear" onclick={onclickClearDownloadedItems}>clear</button>
     </div>
-    <download-list contextmenu={this.context_menu}></download-list>
+    <div class="download-grid-container">
+        <div class="download-grid"></div>
+    </div>
 
     <script>
         /* globals app_base_dir obs */
@@ -35,32 +37,57 @@
         const { Menu } = remote;
         const { SettingStore } = require(`${app_base_dir}/js/setting-store`);
         const { NicoNicoDownloader } = require(`${app_base_dir}/js/niconico-downloader`);
-
-        require(`${app_base_dir}/tags/download-list.tag`);
+        const { GridTableDownloadItem } = require(`${app_base_dir}/js/gridtable-downloaditem`);
 
         const library_dir = SettingStore.getLibraryDir();
+
+        const wait_time = 5;
+        const donwload_state = Object.freeze({
+            wait: 0,
+            downloading: 1,
+            complete: 2,
+            error: 3
+        });
+
+        const message_map = new Map([
+            [donwload_state.wait, "待機"],
+            [donwload_state.downloading, "ダウンロード中"],
+            [donwload_state.complete, "ダウンロード完了"],
+            [donwload_state.error, "ダウンロード失敗"],
+        ]);
+        const htmlFormatter = (row, cell, value, columnDef, dataContext)=> {
+            const msg = message_map.get(value);
+            const content = `<div>${msg}</div><div>${dataContext.progress}</div>`;
+            return content;
+        };
+
+        let grid_table_dl = null;
+        let nico_down = null;
+        let cancel_donwload = false;
+
+        const resizeGridTable = () => {
+            const container = this.root.querySelector(".download-grid-container");
+            grid_table_dl.resizeFitContainer(container);
+        };
 
         const createMenu = () => {
             const nemu_templete = [
                 { label: "delete", click() {
-                    obs.trigger("download-list:delete-selected-items", (deleted_ids)=>{
-                        if(nico_down!=null){
-                            if(deleted_ids.includes(nico_down.video_id)){
-                                nico_down.cancel();
-                            }
-                        }    
-                        obs.trigger("search-page:delete-download-ids", deleted_ids);
-                    });
+                    const deleted_ids = grid_table_dl.deleteSelectedItems();
+                    if(nico_down!=null){
+                        if(deleted_ids.includes(nico_down.video_id)){
+                            nico_down.cancel();
+                        }
+                    } 
+                    obs.trigger("search-page:delete-download-ids", deleted_ids);
                 }},
             ];
             return Menu.buildFromTemplate(nemu_templete);
         };
-        this.context_menu = createMenu();
-
-        let nico_down = null;
-
+        
         this.onclickStartDownload = (e) => {
-            obs.trigger("start-download", async (video_id, on_progress)=>{
+            cancel_donwload = false;
+            startDownload(async (video_id, on_progress)=>{
                 nico_down = new NicoNicoDownloader(video_id, library_dir);
                 const result = await nico_down.download((state)=>{
                     on_progress(state);
@@ -81,15 +108,15 @@
             if(nico_down){
                 nico_down.cancel();
             }
-            obs.trigger("cancel-download");
+            cancel_donwload = true;
         };
 
         this.onclickClearDownloadedItems = () => {
-            obs.trigger("download-list:clear-downloaded-items");  
+            grid_table_dl.clearItems(donwload_state.complete);
         };
 
         obs.on("download-page:add-download-items", (items) => {
-            obs.trigger("download-list:add-download-items", items);  
+            grid_table_dl.addItems(items, donwload_state.wait);
         });
 
         obs.on("download-page:delete-download-items", (video_ids) => {
@@ -97,12 +124,105 @@
                 if(video_ids.includes(nico_down.video_id)){
                     nico_down.cancel();
                 }
-            }   
-            obs.trigger("download-list:delete-download-items", video_ids);  
+            }
+            grid_table_dl.deleteItems(video_ids);  
         });
 
         this.on("mount", () => {
+            grid_table_dl = new GridTableDownloadItem(
+                this.root.querySelector(".download-grid"), htmlFormatter);
             
+            const context_menu = createMenu();
+            try {
+                grid_table_dl.init((e)=>{
+                    context_menu.popup({window: remote.getCurrentWindow()});
+                },(e, data)=>{
+                    obs.trigger("main-page:play-by-videoid", data.id);
+                });                
+            } catch (error) {
+                console.log("donwload item load error=", error);
+            }
+
+            resizeGridTable();            
         });
+
+        // obs.on("set-download-items", (items) => {
+        //     grid_table.setData(items);
+        //     save();
+        // });
+
+        obs.on("get-download-item-callback", (cb) => { 
+            const id_set = grid_table_dl.getItemIDSet();
+            cb(id_set);
+        });
+
+        obs.on("resizeEndEvent", (size)=> {
+            resizeGridTable();
+        });
+
+        const wait = async (do_cancel, on_progress) => {
+            for (let index = wait_time; index >= 0; index--) {
+                if(do_cancel()){
+                    break;
+                }
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                on_progress(`wait ${index}`);
+            }   
+        };
+
+        const startDownload = async(download) => {
+            const first_item = grid_table_dl.getItemByIdx(0);
+            let video_id = first_item.id;
+            while(!cancel_donwload){
+                if(!grid_table_dl.canDownload(video_id, donwload_state.wait)){
+                    video_id = grid_table_dl.getNextVideoID(video_id);
+                    if(video_id===undefined){
+                        break;
+                    }
+                    if(video_id===false){
+                        continue;
+                    }   
+                }
+                await wait(()=>{ return cancel_donwload || !grid_table_dl.hasItem(video_id);}, 
+                    (progress)=>{ 
+                        grid_table_dl.updateItem(video_id, progress, donwload_state.wait);
+                    });
+                if(!grid_table_dl.hasItem(video_id)){
+                    video_id = grid_table_dl.getNextVideoID(video_id);
+                    if(video_id===undefined){
+                        break;
+                    }
+                    continue;
+                }
+                if(cancel_donwload){
+                    grid_table_dl.updateItem(video_id, "cancel", donwload_state.wait);
+                    break;
+                }
+
+                const result = await download(video_id, (progress)=>{ 
+                    grid_table_dl.updateItem(video_id, `${progress}`, donwload_state.downloading);
+                });
+                if(result=="ok"){
+                    grid_table_dl.updateItem(video_id, "finish", donwload_state.complete);
+                }else if(result=="cancel"){
+                    grid_table_dl.updateItem(video_id, "cancel", donwload_state.wait);
+                }else if(result=="skip"){
+                    grid_table_dl.updateItem(video_id, "skip", donwload_state.wait);
+                }else if(result=="error"){
+                    grid_table_dl.updateItem(video_id, "error", donwload_state.error);
+                }
+
+                grid_table_dl.save();
+
+                if(cancel_donwload){
+                    break;
+                }
+
+                video_id = grid_table_dl.getNextVideoID(video_id);
+                if(video_id===undefined){
+                    break;
+                }
+            }
+        };
     </script>
 </download-page>

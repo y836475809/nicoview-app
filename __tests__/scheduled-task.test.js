82212ea12797bdafab8962cdc9ff3b40167118d5
test <test@example.com>
2019-04-17 17:18:34 +0900

    wip: downloader
    scheduled-task test

--- __tests__/scheduled-task.test.js
+++ __tests__/scheduled-task.test.js
@@ -3,15 +3,91 @@ const sinon = require("sinon");
 const { ScheduledTask } = require("../app/js/scheduled-task");
 
 let clock = null;
+let log = [];
+let count = 0;
 test.beforeEach(t => {
     clock = sinon.useFakeTimers();
+    log = [];
+    count = 0;
 });
 
 test.afterEach(t => {
     clock.restore();
 });
 
+const HouerToMsec = (hour) => {
+    return hour*60*60*1000;
+};
 
-test("scheduled task", async (t) => {
-    
+test("scheduled task", t => {
+    const sk = new ScheduledTask(3, ()=>{
+        count++;
+    });
+    sk.on("start", ()=>{log.push("start");});
+    sk.on("execute", ()=>{log.push("execute");});
+    sk.on("stop", ()=>{log.push("stop");});
+
+    sk.start();
+    clock.tick(HouerToMsec(4));
+    sk.stop();
+
+    t.is(count, 1);
+    t.deepEqual(log, ["start","execute","stop"]);
+});
+
+test("scheduled task 0", t => {
+    const sk = new ScheduledTask(3, ()=>{
+        count++;
+    });
+    sk.on("start", ()=>{log.push("start");});
+    sk.on("execute", ()=>{log.push("execute");});
+    sk.on("stop", ()=>{log.push("stop");});
+
+    sk.start();
+    clock.tick(HouerToMsec(2));
+    sk.stop();
+
+    t.is(count, 0);
+    t.deepEqual(log, ["start","stop"]);
+});
+
+test("scheduled task 3", t => {
+    const sk = new ScheduledTask(3, ()=>{
+        count++;
+    });
+    sk.on("start", ()=>{log.push("start");});
+    sk.on("execute", ()=>{log.push("execute");});
+    sk.on("stop", ()=>{log.push("stop");});
+
+    sk.start();
+    clock.tick(HouerToMsec(72));
+    sk.stop();
+
+    t.is(count, 3);
+    t.deepEqual(log, ["start","execute","execute","execute","stop"]);
+});
+
+test("scheduled task m", t => {
+    clock.tick(HouerToMsec(4));
+
+    const sk = new ScheduledTask(3, ()=>{
+        count++;
+    });
+    sk.on("start", ()=>{log.push("start");});
+    sk.on("execute", ()=>{log.push("execute");});
+    sk.on("stop", ()=>{log.push("stop");});
+
+    sk.start();
+
+    clock.tick(HouerToMsec(2));
+    t.is(count, 0);
+
+    clock.tick(HouerToMsec(21));
+    t.is(count, 0);
+
+    clock.tick(HouerToMsec(1/60));
+    sk.stop();
+
+    t.is(count, 1);
+    t.deepEqual(log, ["start","execute","stop"]);
 });
--- app/js/scheduled-task.js
+++ app/js/scheduled-task.js
@@ -1,38 +1,52 @@
 
-class ScheduledTask {
-    constructor(hour, execution){
-        this.hour = hour;
+const EventEmitter = require("events");
+
+class ScheduledTask extends EventEmitter{
+    constructor(scheduled_hour, execution){
+        super();
+        this.scheduled_hour = scheduled_hour;
         this.execution = execution;
         this.timer = null;
     }
 
     start(){
+        this.emit("start");
+
         const rest = this._getRestMinutes()+1;
-        this.timer = setTimeout(this._task, 1000*60*rest);
+        this.timer = setTimeout(()=>{this._task();}, 1000*60*rest);
     }
 
     stop(){        
         if(this.timer){
             clearTimeout(this.timer);
             this.timer = null;  
+
+            this.emit("stop");
         }     
     }
 
     _task(){
         if(this.timer){
+            this.emit("execute");
+
             this.execution();
             const rest = this._getRestMinutes()+1;
-            this.timer = setTimeout(this._task, 1000*60*rest);
+            this.timer = setTimeout(()=>{this._task();}, this._toMsec(rest));
         }
     }
 
     _getRestMinutes(){
-        const d = new Date();
-        const rest = (this.hour-d.getHours())*60 - d.getMinutes();
+        let date = new Date();
+        date = new Date(date.getTime()+this._toMsec(date.getTimezoneOffset()));
+        const rest = (this.scheduled_hour-date.getHours())*60 - date.getMinutes();
         if(rest<0){
             return rest + 24*60;
         } 
         return rest;
+    }
+
+    _toMsec(minute){
+        return minute*60*1000;
     }
 }
 

0f410b1dadbb08a17c317f4abd804d305b1c7b9c
test <test@example.com>
2019-07-04 18:24:52 +0900

    fix test

--- __tests__/xml-data-converter.test.js
+++ __tests__/xml-data-converter.test.js
@@ -1,16 +1,13 @@
 const test = require("ava");
-const { NicoXMLFile, NicoJsonFile } = require("../app/js/nico-data-file");
+const fsPromises = require("fs").promises;
 const { XMLDataConverter } = require("../app/js/xml-data-converter");
 
-//TODO
-
-let nico_json = null;
-
 test.beforeEach(async t => {
-    nico_json = new NicoJsonFile();
-    nico_json.dirPath = "./data";
-    nico_json.commonFilename =  "サンプル1 - [sm1]";
-    nico_json.videoType = "mp4";
+    t.context.xml ={
+        thumbinfo:await fsPromises.readFile(`${__dirname}/data/convert_sample[ThumbInfo].xml`),
+        comments:await fsPromises.readFile(`${__dirname}/data/convert_sample.xml`),
+        owner_comments:await fsPromises.readFile(`${__dirname}/data/convert_sample[Owner].xml`)
+    };
 });
 
 class TestXMLDataConverter extends XMLDataConverter {
@@ -20,10 +17,57 @@ class TestXMLDataConverter extends XMLDataConverter {
 
 test("_convertThumbinfo", async (t) => {
     const cnv_data = new TestXMLDataConverter();
+    const { thumbinfo } = t.context.xml;
 
+    const data = cnv_data._convertThumbinfo(thumbinfo);
+    t.deepEqual(data, {
+        video: {
+            video_id: "sm1000",
+            title: "sample.mp4",
+            description: "投稿コメントサンプル",
+            thumbnailURL: "http://smile?i=1000",
+            largeThumbnailURL: "http://smile?i=1000",
+            postedDateTime: "2000-01-01T01:02:03+09:00",
+            duration: "00:10",
+            viewCount: 100,
+            mylistCount: 5,
+            video_type: "mp4"
+        },
+        thread: {
+            commentCount: 10
+        },
+        tags: [
+            { id: "0", name: "タグ1", isLocked: true},
+            { id: "1", name: "タグ2", isLocked: false},
+        ],
+        owner: {
+            id: "00000",
+            nickname: "ニックネーム",
+            iconURL: "https://blank_s.jpg"
+        }
+    });
 });
 
-test("convertComment", async (t) => {
+test("_convertComment", async (t) => {
     const cnv_data = new TestXMLDataConverter();
+    const { comments, owner_comments } = t.context.xml;
 
+    const data = cnv_data._convertComment(comments, owner_comments);
+    t.deepEqual(data, [
+        {
+            no: 1,
+            vpos: 400,
+            post_date: 0,
+            user_id: "AAA",
+            mail: "naka medium 184",
+            text: "AAAテスト"
+        },
+        {
+            no: 2,
+            vpos: 300,
+            post_date: 1,
+            user_id: "BBB",
+            mail: "184",
+            text: "BBBあ"
+        }]);  
 });

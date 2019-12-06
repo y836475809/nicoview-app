e2cba942d5010ebf9abe41cefad8d69acd1e6543
test <test@example.com>
2019-12-06 17:25:29 +0900

    mod db

--- __tests__/db.test.js
+++ __tests__/db.test.js
@@ -43,30 +43,33 @@ class testDB {
     }
 
     init(){
-        this.p_map = new Map();
-        this.v_map = new Map();
+        this.tables(["path", "video"]);
         this.cmd_log_count = 0;
     }
 
-    setPathData(data_list){
-        this.p_map = rr("path_id", data_list);
-    }
-
-    setVideoData(data_list){
-        this.v_map = rr("video_id", data_list);
+    /**
+     * 
+     * @param {Array<String>} names 
+     */
+    tables(names){
+        this.db_map = new Map();
+        names.forEach(name => {
+            this.db_map.set(name, new Map());
+        });
     }
 
     async load(){
         if(await this._existFile(this.db_path)){ 
             const jsonString = await this._readFile(this.db_path);
             const obj = JSON.parse(jsonString);
-            const r_map = new Map(obj);            
-            this.setPathData(r_map.get("path"));
-            this.setVideoData(r_map.get("video"));
+            const r_map = new Map(obj);  
+            r_map.forEach((value, key)=>{
+                this.db_map.set(key, rr("id", value));
+            }); 
         }
         
         if(await this._existFile(this.log_path)){
-            const cmd_logs = this._readlog(this.log_path);
+            const cmd_logs = await this._readlog(this.log_path);
             this._applyCmdLog(cmd_logs);
             await this.save();
         }
@@ -82,16 +85,18 @@ class testDB {
     }
 
     async addPath(path){
-        for (let [k, v] of this.p_map) {
-            if (v == path) { 
+        const path_map = this.db_map.get("path");
+
+        for (let [k, v] of path_map) {
+            if (v.path == path) { 
                 return k; 
             }
         }  
 
         const max_id = 10000;
         for (let index = 0; index < max_id; index++) {
-            if(!this.p_map.has(index)){
-                this.p_map.set(index, path);
+            if(!path_map.has(index)){
+                path_map.set(index, {"id":index, "path": path});
                 await this._log({
                     "target":"path",
                     "type":"insert",
@@ -105,39 +110,45 @@ class testDB {
     }
 
     exist(video_id){
-        return this.v_map.has(video_id);
+        const video_map = this.db_map.get("video");
+        return video_map.has(video_id);
     }
 
     find(video_id){
-        if(!this.v_map.has(video_id)){
+        const video_map = this.db_map.get("video");
+        if(!video_map.has(video_id)){
             return null;
         }   
-        return this.v_map.get(video_id);
+        return video_map.get(video_id);
     }
 
     findAll(){
-        return Array.from(this.v_map.values()); 
+        const video_map = this.db_map.get("video");
+        return Array.from(video_map.values()); 
     }
 
     async insert(path, video_data){
         const path_id = await this.addPath(path);
         video_data.path_id = path_id;
-        this.v_map.set(video_data.video_id, video_data);
 
+        const video_map = this.db_map.get("video");
+        video_map.set(video_data.id, video_data);
+        
         await this._log({
             "target":"video",
             "type":"insert",
-            "value":{"id":video_data.video_id,"data":video_data}
+            "value":{"id":video_data.id,"data":video_data}
         });
     }
 
     async update(video_id, props){
-        if(!this.v_map.has(video_id)){
+        const video_map = this.db_map.get("video");
+        if(!video_map.has(video_id)){
             throw new Error(`not has ${video_id}`);
         }
 
-        this.v_map.set(video_id, 
-            Object.assign(this.v_map.get(video_id), props));
+        video_map.set(video_id, 
+            Object.assign(video_map.get(video_id), props));
 
         await this._log({
             "target":"video",
@@ -148,12 +159,10 @@ class testDB {
 
     async save(){
         const items = [];
-        Array.from(this.p_map.values()).forEach(value=>{
-            items.push(pp("path", value));
-        });
-        Array.from(this.v_map.values()).forEach(value=>{
-            items.push(pp("video", value));
-        });
+        this.db_map.forEach((value, key)=>{
+            const ary = Array.from(value.values());
+            items.push(pp(key, ary));
+        }); 
 
         const jsonString = `[${items.join(",\n")}]`;
         await this._safeWriteFile(this.db_path , jsonString);
@@ -194,11 +203,11 @@ class testDB {
             return;
         }
         const data = JSON.stringify(cmd, null, 0);
-        await this._appendFile(this.log_path, data, "utf-8");
+        await this._appendFile(this.log_path, `${data}\n`, "utf-8");
     }
 
     async _readlog(log_path){
-        if(!this._existFile(log_path)){
+        if(!await this._existFile(log_path)){
             return [];
         }
 
@@ -235,22 +244,24 @@ class testDB {
     _applyCmdLog(cmd_logs){
         cmd_logs.forEach(item=>{
             if(item.target=="path"){
+                const path_map = this.db_map.get("path");
                 const value = item.value;
                 if(item.type=="insert"){
-                    this.p_map.set(value.id, value);
+                    path_map.set(value.id, value);
                 }
             }
             if(item.target=="video"){
+                const video_map = this.db_map.get("video");
                 const value = item.value;
                 if(item.type=="insert"){
-                    this.v_map.set(value.id, value);
+                    video_map.set(value.id, value);
                 }
                 if(item.type=="update"){
-                    if(!this.v_map.has(value.id)){
+                    if(!video_map.has(value.id)){
                         return;
                     }
-                    this.v_map.set(value.id, 
-                        Object.assign(this.v_map.get(value.id), value.data));
+                    video_map.set(value.id, 
+                        Object.assign(video_map.get(value.id), value.data));
                 }
             }
         }); 
@@ -262,11 +273,11 @@ test("db1", async t => {
     db.init();
     await db.load();
     await db.insert("c:/data", 
-        {"video_id":"sm1","path_id":-1,"is_economy":false,"time":100,"tags":["タグ1"]});
+        {"id":"sm1","path_id":-1,"is_economy":false,"time":100,"tags":["タグ1"]});
     await db.insert("c:/data", 
-        {"video_id":"sm2","path_id":-1,"is_economy":false,"time":100,"tags":["タグ1"]});
+        {"id":"sm2","path_id":-1,"is_economy":false,"time":100,"tags":["タグ1"]});
     await db.insert("c:/data/test", 
-        {"video_id":"sm3","path_id":-1,"is_economy":false,"time":100,"tags":["タグ1"]});
+        {"id":"sm3","path_id":-1,"is_economy":false,"time":100,"tags":["タグ1"]});
 
     console.log( db.find("sm2"));
     console.log( db.find("sm3"));

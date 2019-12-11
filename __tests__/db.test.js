b3c6db0d829f4b4bb82c1eaf27adf71c1dc25f10
y836475809 <tttdaytttday@gmail.com>
2019-12-12 00:48:21 +0900

    add db.js

--- __tests__/db.test.js
+++ __tests__/db.test.js
@@ -1,306 +1,10 @@
 const test = require("ava");
-const fs = require("fs").promises;
 const path = require("path");
 
-class testDB {
-    constructor({filename="./db.json", autonum=10, use_log=true}={}){  
-        this.autonum = autonum;
-        this.use_log = use_log;
-        const fullpath = path.resolve(filename);
-        this.db_path = fullpath;
-        this.log_path = this._getLogFilePath(fullpath);
+const { MapDB, LibraryDB } = require("../app/js/db");
 
-        // this.init();
-        this.db_map = new Map();
-        this.cmd_log_count = 0;
-    }
 
-    _getLogFilePath(db_file_path){
-        const dir = path.dirname(db_file_path);
-        const ext = path.extname(db_file_path);
-        return path.join(dir, `${path.basename(db_file_path, ext)}.log`);
-    }
-
-    createTable(names){
-        this.db_map.clear();
-        names.forEach(name => {
-            this.db_map.set(name, new Map());
-        });
-    }
-
-    async load(){
-        if(await this._existFile(this.db_path)){ 
-            const jsonString = await this._readFile(this.db_path);
-            const obj = JSON.parse(jsonString);
-            const r_map = new Map(obj);  
-            r_map.forEach((value, key)=>{
-                this.db_map.set(key, this._convertMap("id", value));
-            }); 
-        }
-        
-        if(await this._existFile(this.log_path)){
-            const cmd_logs = await this._readlog(this.log_path);
-            this._applyCmdLog(cmd_logs);
-            await this.save();
-        }
-    }
-        
-    /**
-     * 
-     * @param {String} name 
-     * @param {Array} ary 
-     */
-    _convertMap(name, ary){
-        const map = new Map();
-        ary.forEach(value=>{
-            map.set(value[name], value);
-        });
-        return map;
-    };
-
-    exist(name, id){
-        if(!this.db_map.has(name)){
-            return false;
-        }
-        return this.db_map.get(name).has(id);
-    }
-
-    find(name, id){
-        if(!this.db_map.has(name)){
-            return null;
-        }
-        const map = this.db_map.get(name);
-        if(!map.has(id)){
-            return null;
-        }   
-        return map.get(id);
-    }
-
-    findAll(name){
-        if(!this.db_map.has(name)){
-            return [];
-        }
-        const map = this.db_map.get(name);
-        return Array.from(map.values()); 
-    }
-
-    async insert(name, data){
-        const map = this.db_map.get(name);
-        map.set(data.id, data);
-        
-        await this._log({
-            target:name,
-            type:"insert",
-            value:{id:data.id,data:data}
-        });
-    }
-
-    async update(name, id, props){
-        const map = this.db_map.get(name);
-        if(!map.has(id)){
-            throw new Error(`${name} not has ${id}`);
-        }
-
-        map.set(id, 
-            Object.assign(map.get(id), props));
-
-        await this._log({
-            target:name,
-            type:"update",
-            value:{id:id, data:props}
-        });
-    }
-
-    async save(){
-        const items = [];
-        this.db_map.forEach((value, key)=>{
-            const ary = Array.from(value.values());
-            items.push(this._convertString(key, ary));
-        }); 
-
-        const jsonString = `[${items.join(",\n")}]`;
-        await this._safeWriteFile(this.db_path , jsonString);
-        await this._deletelog();
-        this.cmd_log_count = 0;
-    }
-
-    _convertString(key, ary){
-        const output = ary.map(value=>{
-            return JSON.stringify(value, null, 0);
-        }).join(",\n");
-    
-        return `["${key}", [\n${output}\n]]`;
-    };
-
-    async _existFile(file_path){
-        try {
-            await fs.stat(file_path);
-            return true;
-        } catch (error) {
-            return false;
-        }  
-    }
-
-    async _safeWriteFile(file_path, data){
-        const tmp_path = path.join(path.dirname(file_path), `~${path.basename(file_path)}`);
-        await this._writeFile(tmp_path, data, "utf-8");    
-        await this._rename(tmp_path, file_path);
-    }
-
-    async _log(cmd){
-        if(!this.use_log){
-            return;
-        }
-
-        this.cmd_log_count++;
-        await this._writelog(cmd);
-
-        if(this.cmd_log_count >= this.autonum){
-            await this.save();      
-            return;
-        }
-    }
-
-    async _deletelog(){
-        await this._unlink(this.log_path);
-    } 
-
-    async _writelog(cmd){
-        const data = JSON.stringify(cmd, null, 0);
-        await this._appendFile(this.log_path, `${data}\n`);
-    }
-
-    async _readlog(log_path){
-        if(!await this._existFile(log_path)){
-            return [];
-        }
-
-        const str = await this._readFile(log_path);
-        const lines = str.split(/\r\n|\n/);
-        const cmds = lines.filter(line=>{
-            return line.trim().length > 0;
-        }).map(line=>{
-            return JSON.parse(line.trim());
-        });
-        return cmds;
-    }
-
-    async _readFile(file_path){
-        return await fs.readFile(file_path, "utf-8");
-    }
-    async _appendFile(file_path, data){
-        await fs.appendFile(file_path, data, "utf-8");
-    }
-    async _unlink(file_path){
-        return await fs.unlink(file_path);
-    }
-    async _writeFile(file_path, data){
-        await fs.writeFile(file_path, data, "utf-8");    
-    }
-    async _rename(old_path, new_path){
-        await fs.rename(old_path, new_path);
-    }
-
-    /**
-     * 
-     * @param {Array} cmd_logs 
-     */
-    _applyCmdLog(cmd_logs){
-        cmd_logs.forEach(item=>{
-            const target = item.target;
-            if(!this.db_map.has(target)){
-                return;
-            }
-
-            const data_map = this.db_map.get(target);
-            const value = item.value;
-            if(item.type=="insert"){
-                data_map.set(value.id, value.data);
-            }
-
-            if(item.type=="update"){
-                if(!data_map.has(value.id)){
-                    return;
-                }
-                data_map.set(value.id, 
-                    Object.assign(data_map.get(value.id), value.data));
-            }
-        }); 
-    }
-}
-
-class kk {
-    constructor({db_file_path="./db.json", autonum=10}={}){
-        // this._db = this._createDB({filename:db_file_path, autonum:autonum});
-        // this._db.createTable(["path, video"]);
-        this.params = {filename:db_file_path, autonum:autonum};
-    }
-
-    _createDB(params){
-        return new testDB(params);
-    }
-
-    async load(){
-        this._db = this._createDB(this.params);
-        this._db.createTable(["path, video"]);
-        await this._db.load();
-    }
-
-    exist(video_id){
-        return this._db.exist("vieo", video_id);
-    }
-
-    find(video_id){ 
-        const video_item = this._db.find("video", video_id);
-        const path_item = this._db.find("path", video_item.path_id);
-        video_item.path = path_item.path;
-        // Object.assign(vieo_item, {path: path});
-        return video_item;
-    }
-
-    findAll(){
-        const video_items = this._db.findAll("video");
-        video_items.forEach(item=>{
-            const path_item = this._db.find("path", item.path_id);
-            item.path = path_item.path;
-        });
-        return video_items;
-        // return this._db.findAll("video");
-    }
-
-    async insert(path, video_data){
-        const path_id = await this._getPathID(path);
-        video_data.path_id = path_id;
-
-        await this._db.insert("path", {"id":path_id,"path":path});
-        await this._db.insert("video", video_data);
-    }
-
-    async update(video_id, props){
-        await this._db.update("video", video_id, props);
-    }
-
-    async _getPathID(path){
-        const path_map = this._db.db_map.get("path");
-        for (let [k, v] of path_map) {
-            if (v.path == path) { 
-                return k; 
-            }
-        }  
-
-        const max_id = 10000;
-        for (let index = 0; index < max_id; index++) {
-            const id = String(index);
-            if(!path_map.has(id)){
-                return id;
-            } 
-        }
-
-        throw new Error("maximum id value has been exceeded");
-    }
-}
-
-class testDB2 extends testDB {
+class testMapDB extends MapDB {
     constructor(exist_log=false){
         super({autonum:2});
         this.test_log = [];
@@ -357,20 +61,20 @@ class testDB2 extends testDB {
     }
 } 
 
-class testDB3 extends kk {
+class testLibraryDB extends LibraryDB {
     constructor(exist_log=false){
         super({autonum:2});
         this.exist_log = exist_log;
     }
 
     _createDB(params){
-        return new testDB2(this.exist_log);
+        return new testMapDB(this.exist_log);
     }
 } 
 
 
 test("db non", async t => {
-    const db = new testDB({use_log:false});
+    const db = new MapDB({use_log:false});
 
     t.falsy(db.exist("sm1"));
     t.is(db.find("sm1"), null);
@@ -378,7 +82,7 @@ test("db non", async t => {
 });
 
 test("db1", async t => {
-    const db = new testDB({use_log:false});
+    const db = new MapDB({use_log:false});
     db.createTable(["p", "v"]);
 
     const p_list = [
@@ -427,7 +131,7 @@ test("db1", async t => {
 
 test("db2", async t => {
     const filename = path.join(__dirname, "test.join");
-    const db = new testDB({filename:filename});
+    const db = new MapDB({filename:filename});
 
     t.is(db.db_path, filename);
     t.is(db.log_path, path.join(__dirname, "test.log"));
@@ -438,7 +142,7 @@ test("db3", async t => {
     const db_tmp_fname = "~db.json";
     const log_fname = "db.log";
 
-    const db = new testDB2();
+    const db = new testMapDB();
     await db.load();
 
     await db.insert("path", {id:1, path:1});
@@ -468,7 +172,7 @@ test("db4", async t => {
     const log_fname = "db.log";
 
     const exist_log = true;
-    const db = new testDB2(exist_log);
+    const db = new testMapDB(exist_log);
     await db.load();
 
     t.deepEqual(db.findAll("path"), [
@@ -489,7 +193,7 @@ test("db4", async t => {
 });
 
 test("db path", async t => {
-    const db = new testDB3();
+    const db = new testLibraryDB();
     await db.load();
 
     await db.insert("c:/data", {id:"sm2"});
@@ -504,7 +208,7 @@ test("db path", async t => {
 
 test("db log", async t => {
     const exist_log = true;
-    const db = new testDB3(exist_log);
+    const db = new testLibraryDB(exist_log);
     await db.load();
 
     t.deepEqual(db.findAll(), [

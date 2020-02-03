c507e609e420024f5bb647762dba1fde319be828
y836475809 <tttdaytttday@gmail.com>
2020-02-04 00:41:10 +0900

    wip: add test

--- __tests__/import-library-file.test.js
+++ __tests__/import-library-file.test.js
@@ -1,4 +1,6 @@
 const test = require("ava");
+const path = require('path');
+const { NicoXMLFile, NicoJsonFile } = require("../app/js/nico-data-file");
 
 test("regx param", t => {
     // t.truthy(/sm\d+\.[mp4|flv|swf]/.test("sm10.mp4"));
@@ -13,21 +15,37 @@ test("regx param", t => {
     // t.falsy(/\[sm\d+\]\.[mp4|flv|swf]/.test("sm10.swf"));
     // t.truthy(/\[sm\d+\]\.[mp4|flv|swf]/.test("サンプル1 - [sm10].mp4")); 
     // t.truthy(/\[sm\d+\]\.[mp4|flv|swf]/.test("サンプル1 - [sm10].flv"));
-    // t.truthy(/\[sm\d+\]\.[mp4|flv|swf]/.test("サンプル1 - [sm10].swf"));   
+    // t.truthy(/\[sm\d+\]\.[mp4|flv|swf]/.test("サンプル1 - [sm10].swf"));  
+    const fname1 = "sm10.mp4"; 
+    const fname2 = "サンプル1 - [sm10].mp4";
 
-    const m1 = /(sm\d+)\.(mp4|flv|swf)/.exec("sm10.flv");
+    const m1 = /(sm\d+)\.(mp4|flv|swf)/.exec(fname1);
     console.log(m1)
     t.is(m1[1], "sm10");
-    const m2 = /(sm\d+)\.(mp4|flv|swf)/.exec("サンプル1 - [sm10].mp4");
+    t.is(path.basename(fname1, path.extname(fname1)), "sm10");
+    const m2 = /(sm\d+)\.(mp4|flv|swf)/.exec(fname2);
     t.is(m2, null);
 
-    const m3 = /\[(sm\d+)\]\.(mp4|flv|swf)/.exec("sm10.mp4");
+    const m3 = /\[(sm\d+)\]\.(mp4|flv|swf)/.exec(fname1);
     t.is(m3, null);
-    const m4 = /\[(sm\d+)\]\.(mp4|flv|swf)/.exec("サンプル1 - [sm10].mp4");
+    const m4 = /\[(sm\d+)\]\.(mp4|flv|swf)/.exec(fname2);
     t.is(m4[1], "sm10");
+    t.is(path.basename(fname2, path.extname(fname2)), "サンプル1 - [sm10]");
     // "sm10.flv";
     // "sm10.swf";
     // "サンプル1 - [sm10].mp4";
     // "サンプル1 - [sm10].flv";
     // "サンプル1 - [sm10].swf";
 });
+
+// test("file", t => {
+//     const video_item = {
+//         data_type:"",
+//         dirpath:,
+//         common_filename:,
+//         video_type:,
+//         thumbnail_size:,
+//         is_deleted
+//     };
+//     const nd = new NicoVideoData();
+// });

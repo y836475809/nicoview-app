ea727d19b637ea62053987a116df95b61e642e1b
y836475809 <tttdaytttday@gmail.com>
2020-04-07 10:13:43 +0900

    convert nndd data(json to xml)

---
+++ __tests__/json-data-converter.test.js
@@ -0,0 +1,189 @@
+const test = require("ava");
+const path = require("path");
+const { JsonDataConverter } = require("../app/js/nico-data-converter");
+const owner_comment_json = require("./data/owner-comment.json");
+const no_owner_comment_json = require("./data/no-owner-comment.json");
+
+class TestPathJsonDataConverter extends JsonDataConverter{
+    constructor(video_item){
+        super(video_item);
+        this.paths = [];
+    }
+
+    _convertThumbinfo(json){
+        return "";
+    }
+
+    _convertComment(comment_json){
+        return "";
+    }
+
+    async _read(file_path){
+        this.paths.push(file_path);
+        return "[]";
+    }
+
+    async _write(file_path, data){
+        this.paths.push(file_path);
+    }
+
+    async _copyFile(src_file_path, dist_file_path){
+        this.paths.push(src_file_path);
+        this.paths.push(dist_file_path);
+    }   
+}
+
+test.beforeEach(t => {
+    const video_item = {
+        data_type: "json", 
+        id: "sm10",
+        dirpath_id: 1,
+        dirpath: __dirname,
+        video_name: "test title",
+        common_filename: "test",
+        video_type: "mp4",
+        is_deleted: false,
+        tags: [],
+        thumbnail_size: "L"
+    };
+
+    const video_item_s = Object.assign({}, video_item);
+    video_item_s.thumbnail_size = "S";
+
+    t.context.cnv_data = new JsonDataConverter(video_item);
+    t.context.path_cnv_data = new TestPathJsonDataConverter(video_item);
+    t.context.path_cnv_data_S = new TestPathJsonDataConverter(video_item_s);
+});
+
+test("_convertDate", t => {
+    const cnv_data = t.context.cnv_data;
+    t.is(
+        cnv_data._convertDate("2000/10/02 03:40:50"),
+        "2000-10-02T03:40:50+09:00");
+});
+
+test("_convertDescription", t => {
+    const cnv_data = t.context.cnv_data;
+    const json_desc = 
+        'test<a href="https://www.nicovideo.jp/watch/sm10" class="watch">sm10</a>'
+        +"& &lt;　&gt;<br>"
+        +'<a href="https://www.nicovideo.jp/mylist/10" target="_blank" rel="noopener">mylist/10</a>'; 
+    t.is(
+        cnv_data._convertDescription(json_desc),
+        "test&lt;a href=&quot;https://www.nicovideo.jp/watch/sm10&quot; class=&quot;watch&quot;&gt;sm10&lt;/a&gt;"
+        +"&amp; &amp;lt;　&amp;gt;&lt;br&gt;"
+        +"&lt;a href=&quot;https://www.nicovideo.jp/mylist/10&quot; target=&quot;_blank&quot; rel=&quot;noopener&quot;&gt;mylist/10&lt;/a&gt;");
+});
+
+
+test("_convertTags", t => {
+    const cnv_data = t.context.cnv_data;
+    const tags = cnv_data._convertTags([
+        { name: "タグ1", isLocked: true, category: true},
+        { name: "タグ2", isLocked: true},
+        { name: "タグ3", isLocked: false},
+    ]);
+    t.is(tags, 
+        [
+            '<tag category="1" lock="1">タグ1</tag>',
+            '<tag lock="1">タグ2</tag>',
+            "<tag>タグ3</tag>",
+        ].join("\r\n")
+    );
+});
+
+test("_convertComment owner_comment", t => {
+    const cnv_data = t.context.cnv_data;
+    const { owner_comment_xml, user_comment_xml } = 
+        cnv_data._convertComment(owner_comment_json);
+    
+    t.is(owner_comment_xml, 
+        [
+            "<packet>",
+            '<thread resultcode="0" thread="01234567890" last_res="2" ticket="0x1234567890" revision="1" fork="1" server_time="1546694359"/>',
+            '<chat thread="01234567890" no="1" vpos="100" date="1360996778" mail="shita green small" fork="1">owner comment1\ncomment1</chat>',
+            '<chat thread="01234567890" no="2" vpos="200" date="1360996778" mail="shita green small" fork="1">owner comment2</chat>',
+            "</packet>",
+        ].join("\r\n")
+    );
+    t.is(user_comment_xml, 
+        [
+            "<packet>",
+            '<thread resultcode="0" thread="1234567890" last_res="2" ticket="0x83d23581" revision="2" server_time="1546694359"/>',
+            '<chat thread="1234567890" no="16" vpos="300" date="1359607148" mail="184" user_id="abcdefg">comment3</chat>',
+            '<chat thread="1234567890" no="17" vpos="400" date="1359607224" mail="184" user_id="hijklnm">comment4</chat>',
+            "</packet>",
+        ].join("\r\n")
+    );
+});
+
+test("_convertComment no_owner_comment", t => {
+    const cnv_data = t.context.cnv_data;
+    const { owner_comment_xml, user_comment_xml } = 
+        cnv_data._convertComment(no_owner_comment_json);
+    
+    t.is(owner_comment_xml, 
+        [
+            "<packet>",
+            '<thread resultcode="0" thread="1234567890" last_res="0" ticket="0" revision="1" fork="1" server_time="1546694145"/>',
+            "",
+            "</packet>",
+        ].join("\r\n")
+    );
+    t.is(user_comment_xml, 
+        [
+            "<packet>",
+            '<thread resultcode="0" thread="1234567890" last_res="2" ticket="0xb18d801d" revision="1" server_time="1546694145"/>',
+            '<chat thread="1234567890" no="1" vpos="100" date="1522161709" mail="184" user_id="abcdefg">comment1</chat>',
+            '<chat thread="1234567890" no="2" vpos="200" date="1522161986" mail="184" user_id="hijklmn">comment2</chat>',
+            "</packet>",
+        ].join("\r\n")
+    );
+});
+
+test("paths thumbnail L", async t => {
+    const cnv_data = t.context.path_cnv_data;
+   
+    await cnv_data.convertThumbInfo();
+    t.deepEqual(cnv_data.paths, [
+        path.join(__dirname, "test - [sm10][ThumbInfo].json"),
+        path.join(__dirname, "test - [sm10][ThumbInfo].xml")
+    ]);
+    cnv_data.paths = [];
+
+    await cnv_data.convertComment();
+    t.deepEqual(cnv_data.paths, [
+        path.join(__dirname, "test - [sm10][Comment].json"),
+        path.join(__dirname, "test - [sm10][Owner].xml"),
+        path.join(__dirname, "test - [sm10].xml")
+    ]);
+    cnv_data.paths = [];
+    
+    await cnv_data.convertThumbnai();
+    t.deepEqual(cnv_data.paths, [
+        path.join(__dirname, "test - [sm10][ThumbImg].L.jpeg"),
+        path.join(__dirname, "test - [sm10][ThumbImg].jpeg")
+    ]);
+});
+
+test("paths thumbnail S", async t => {
+    const cnv_data = t.context.path_cnv_data_S;
+   
+    await cnv_data.convertThumbInfo();
+    t.deepEqual(cnv_data.paths, [
+        path.join(__dirname, "test - [sm10][ThumbInfo].json"),
+        path.join(__dirname, "test - [sm10][ThumbInfo].xml")
+    ]);
+    cnv_data.paths = [];
+
+    await cnv_data.convertComment();
+    t.deepEqual(cnv_data.paths, [
+        path.join(__dirname, "test - [sm10][Comment].json"),
+        path.join(__dirname, "test - [sm10][Owner].xml"),
+        path.join(__dirname, "test - [sm10].xml")
+    ]);
+    cnv_data.paths = [];
+    
+    await cnv_data.convertThumbnai();
+    t.deepEqual(cnv_data.paths, []);
+});
--- app/js/nico-data-converter.js
+++ app/js/nico-data-converter.js
@@ -1,5 +1,7 @@
 const fsPromises = require("fs").promises;
 const NicoDataParser = require("./nico-data-parser");
+const { NicoXMLFile, NicoJsonFile } = require("./nico-data-file");
+const { toTimeString } = require("./time-format");
 
 class XMLDataConverter {
     async convertThumbInfo(nico_xml, nico_json){
@@ -72,7 +74,236 @@ class XMLDataConverter {
     }
 }
 
+class JsonDataConverter {
+    constructor(video_item){
+        const { nico_xml, nico_json } = this._getNicoFileData(video_item);
+        this.nico_xml = nico_xml;
+        this.nico_json = nico_json;
+    }
+    async convertThumbInfo(){
+        const thumbinfo_json = await this._read(this.nico_json.thumbInfoPath);
+        await this._write(this.nico_xml.thumbInfoPath, this._convertThumbinfo(thumbinfo_json));
+    }
+
+    async convertComment(){
+        const comment_json = JSON.parse(await this._read(this.nico_json.commentPath));
+        const { owner_comment_xml, user_comment_xml } = this._convertComment(comment_json);
+        await this._write(this.nico_xml.ownerCommentPath, owner_comment_xml);
+        await this._write(this.nico_xml.commentPath, user_comment_xml);
+    }
+
+    async convertThumbnai(){
+        if(this.nico_json.thumbImgPath == this.nico_xml.thumbImgPath){
+            return;
+        }
+        await this._copyFile(this.nico_json.thumbImgPath, this.nico_xml.thumbImgPath);
+    }
+
+    _getNicoFileData(video_item){
+        const nico_xml = new NicoXMLFile(video_item.id);
+        nico_xml.dirPath = video_item.dirpath;
+        nico_xml.commonFilename = video_item.common_filename;
+
+        const nico_json = new NicoJsonFile(video_item.id);
+        nico_json.dirPath = video_item.dirpath;
+        nico_json.commonFilename = video_item.common_filename;
+        nico_json.thumbnailSize = video_item.thumbnail_size;
+
+        return { nico_xml, nico_json };
+    }
+
+    /**
+     * 
+     * @param {Array} comment_json 
+     */
+    _convertComment(comment_json){
+        const threads = comment_json.filter(value => {
+            return value.hasOwnProperty("thread");
+        }).map(value => {
+            return value.thread;
+        });
+        const comments = comment_json.filter(value => {
+            return value.hasOwnProperty("chat") && !value.chat.hasOwnProperty("deleted");
+        }).map(value => {
+            return value.chat;
+        });
+
+        const owner_threads = threads.filter(value => {
+            return value.hasOwnProperty("fork");
+        });
+        const user_threads = threads.filter(value => {
+            return !value.hasOwnProperty("fork");
+        });
+
+        const owner_comments = comments.filter(value => {
+            return value.hasOwnProperty("fork");
+        });
+        const user_comments = comments.filter(value => {
+            return !value.hasOwnProperty("fork");
+        });
+
+        const user_thread = {
+            thread: user_threads[0].thread,
+            last_res: user_comments.length,
+            ticket: user_threads[0].ticket,
+            revision: user_threads[0].revision,
+            server_time: user_threads[0].server_time,  
+        };
+        const owner_thread = Object.assign({}, user_thread);
+        owner_thread.fork = 1;
+
+        if(owner_threads.length==0){
+            owner_thread.ticket = 0;
+            owner_thread.last_res = 0;
+        }else{
+            owner_thread.thread = owner_threads[0].thread;
+            owner_thread.last_res = owner_comments.length;
+            owner_thread.ticket = owner_threads[0].ticket;
+            owner_thread.revision = owner_threads[0].revision;
+            owner_thread.server_time = owner_threads[0].server_time;
+        }
+
+        const owner_comment_xml = [
+            "<packet>",
+            `<thread resultcode="0" thread="${owner_thread.thread}" last_res="${owner_thread.last_res}" ticket="${owner_thread.ticket}" revision="${owner_thread.revision}" fork="1" server_time="${owner_thread.server_time}"/>`,
+            owner_comments.map(value => {
+                const no = value.no;
+                const vpos = value.vpos;
+                const date = value.date;
+                const mail = value.mail;
+                const content = value.content;
+                return `<chat thread="${owner_thread.thread}" no="${no}" vpos="${vpos}" date="${date}" mail="${mail}" fork="1">${content}</chat>`;
+            }).join("\r\n"),
+            "</packet>"
+        ].join("\r\n");
+
+        const user_comment_xml = [
+            "<packet>",
+            `<thread resultcode="0" thread="${user_thread.thread}" last_res="${user_thread.last_res}" ticket="${user_thread.ticket}" revision="${user_thread.revision}" server_time="${user_thread.server_time}"/>`,
+            user_comments.map(value => {
+                const no = value.no;
+                const vpos = value.vpos;
+                const date = value.date;
+                const mail = value.mail;
+                const user_id = value.user_id;
+                const content = value.content;
+                return `<chat thread="${user_thread.thread}" no="${no}" vpos="${vpos}" date="${date}" mail="${mail}" user_id="${user_id}">${content}</chat>`;
+            }).join("\r\n"),
+            "</packet>"
+        ].join("\r\n");
+
+        return { owner_comment_xml, user_comment_xml };
+    }
+
+    _convertDate(date_str){
+        const d = new Date(date_str);
+
+        const year = d.getFullYear();
+        const month = ("0" + (d.getMonth() + 1)).slice(-2);
+        const day = ("0" + d.getDate()).slice(-2);
+        const hour = ("0" + d.getHours()).slice(-2);
+        const min = ("0" + d.getMinutes()).slice(-2);
+        const sec = ("0" + d.getSeconds()).slice(-2);
+
+        const offset = -d.getTimezoneOffset();
+        const offset_hour = ("0" + Math.floor(offset / 60)).slice(-2);
+        const offset_min = ("0" + Math.floor(offset % 60)).slice(-2);
+
+        return `${year}-${month}-${day}T${hour}:${min}:${sec}+${offset_hour}:${offset_min}`;
+    }
+
+    /**
+     * 
+     * @param {String} description 
+     */
+    _convertDescription(description){
+        return description
+            .replace(/&/g, "&amp;")
+            .replace(/>/g, "&gt;")
+            .replace(/</g, "&lt;")
+            .replace(/"/g, "&quot;");
+    }
+
+    /**
+     * 
+     * @param {Array} tags 
+     */
+    _convertTags(tags, prefix=""){
+        return tags.map(value => {
+            const lock = value.isLocked===true?" lock=\"1\"":"";
+            const category = value.category===true?" category=\"1\"":"";
+            return `${prefix}<tag${category}${lock}>${value.name}</tag>`;
+        }).join("\r\n");
+    }
+
+    _convertThumbinfo(json){
+        const thumb_info = JSON.parse(json);
+        const video = thumb_info.video;
+        const video_id = video.video_id;
+        const title = video.title;
+        const description = this._convertDescription(video.description);
+        const thumbnail_url = video.thumbnailURL;
+        const first_retrieve = this._convertDate(video.postedDateTime);
+        const time = toTimeString(video.duration);
+        const movie_type = video.video_type;
+        const view_counter = video.viewCount;
+
+        const comment_num = thumb_info.thread.commentCount;
+        const mylist_counter = video.mylistCount;
+
+        const user_id       = thumb_info.owner.id;
+        const user_nickname = thumb_info.owner.nickname;
+        const user_icon_url = thumb_info.owner.iconURL;
+
+        const tags = this._convertTags(thumb_info.tags, "      ");
+
+        const xml = [
+            "<nicovideo_thumb_response status=\"ok\">",
+            "  <thumb>",
+            `    <video_id>${video_id}</video_id>`,
+            `    <title>${title}</title>`,
+            `    <description>${description}</description>`,
+            `    <thumbnail_url>${thumbnail_url}</thumbnail_url>`,
+            `    <first_retrieve>${first_retrieve}</first_retrieve>`,
+            `    <length>${time}</length>`,
+            `    <movie_type>${movie_type}</movie_type>`,
+            "    <size_high>0</size_high>",
+            "    <size_low>0</size_low>",
+            `    <view_counter>${view_counter}</view_counter>`,
+            `    <comment_num>${comment_num}</comment_num>`,
+            `    <mylist_counter>${mylist_counter}</mylist_counter>`,
+            "    <last_res_body></last_res_body>",
+            `    <watch_url>https://www.nicovideo.jp/watch/${video_id}</watch_url>`,
+            "    <thumb_type>video</thumb_type>",
+            "    <embeddable>1</embeddable>",
+            "    <no_live_play>0</no_live_play>",
+            "    <tags domain=\"jp\">",
+            `${tags}`,
+            "    </tags>",
+            `    <user_id>${user_id}</user_id>`,
+            `    <user_nickname>${user_nickname}</user_nickname>`,
+            `    <user_icon_url>${user_icon_url}</user_icon_url>`,
+            "  </thumb>",
+            "</nicovideo_thumb_response>"
+        ].join("\r\n");
+
+        return xml;
+    }
+
+    async _read(file_path){
+        return await fsPromises.readFile(file_path, "utf-8");
+    }
+
+    async _write(file_path, data){
+        await fsPromises.writeFile(file_path, data, "utf-8");
+    }
+
+    async _copyFile(src_file_path, dist_file_path){
+        await fsPromises.copyFile(src_file_path, dist_file_path);
+    }
+}
 
 module.exports = {
-    XMLDataConverter
+    XMLDataConverter,
+    JsonDataConverter
 };

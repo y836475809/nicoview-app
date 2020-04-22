3644967ab5017ac449947f9d4e9d33f31f74e596
y836475809 <tttdaytttday@gmail.com>
2020-04-23 08:36:14 +0900

    clean

--- app/preload_main.js
+++ app/preload_main.js
@@ -1,24 +1,13 @@
-const electron = require("electron");
-const path = require("path");
-const EventEmitter = require("events");
-const Sortable = require("sortablejs");
-
-const root_dir = path.resolve(__dirname, "..");
-
-const IPC_CHANNEL = require(`${root_dir}/app/js/ipc-channel`);
-const DataIpc = require(`${root_dir}/app/js/data-ipc`);
-const logger = require(`${root_dir}/app/js/logger`);
+const root_dir = "..";
 
 process.once("loaded", () => {
     global.process = process;
-    global.electron = electron;
-    global.path = path;
-    global.module = module;
-    global.EventEmitter = EventEmitter;
-
-    global.IPC_CHANNEL = IPC_CHANNEL;
-    global.DataIpc = DataIpc;
-    global.logger = logger;
+    global.electron = require("electron");
+    global.path = require("path");
+    global.EventEmitter = require("events");
+    global.IPC_CHANNEL = require(`${root_dir}/app/js/ipc-channel`);
+    global.DataIpc = require(`${root_dir}/app/js/data-ipc`);
+    global.logger = require(`${root_dir}/app/js/logger`);
 
     if (process.env.NODE_ENV == "DEBUG") {
         global.NicoMock = require(`${root_dir}/test/mock_server/nico-mock`);
@@ -32,7 +21,7 @@ process.once("loaded", () => {
 window.addEventListener( "error", async e => {
     const { message, filename, lineno, colno } = e;
     const msg = `${message}\n${filename}:${lineno}`;
-    logger.error(msg);
+    global.logger.error(msg);
 
     try {
         await global.RemoteDailog.showMessageBox("error", msg);
@@ -42,7 +31,7 @@ window.addEventListener( "error", async e => {
 } );
 
 window.addEventListener( "unhandledrejection", async e => {
-    logger.error(e.reason);
+    global.logger.error(e.reason);
    
     try {
         await global.RemoteDailog.showMessageBox("error", e.reason.message);
@@ -53,43 +42,24 @@ window.addEventListener( "unhandledrejection", async e => {
 
 window.addEventListener("load", () => {
     window.$ = window.jQuery = require("jquery");
-
     require("slickgrid/plugins/slick.autotooltips");
-    const GridTable = require(`${root_dir}/app/js/gridtable`);
-    const RemoteDailog = require(`${root_dir}/app/js/remote-dialogs`);
-    const TimeFormat = require(`${root_dir}/app/js/time-format`);
-    const NicoSearch = require(`${root_dir}/app/js/nico-search`);
-    const NicoVideoData = require(`${root_dir}/app/js/nico-data-file`);
-    const NicoMylist = require(`${root_dir}/app/js/nico-mylist`);
-    const NicoUpdate = require(`${root_dir}/app/js/nico-update`);
-    const History = require(`${root_dir}/app/js/history`);
-    const JsonStore = require(`${root_dir}/app/js/json-store`);
-    const BookMark = require(`${root_dir}/app/js/bookmark`);
-    const CacheStore = require(`${root_dir}/app/js/cache-store`);
-    const VideoConverter = require(`${root_dir}/app/js/video-converter`);
-    const NicoDownloader = require(`${root_dir}/app/js/nico-downloader`);
-    const GridTableDownloadItem = require(`${root_dir}/app/js/gridtable-downloaditem`);
-    const ScheduledTask = require(`${root_dir}/app/js/scheduled-task`);
-    const ImportLibrary = require(`${root_dir}/app/js/import-library`);
-    const NicoDataConverter = require(`${root_dir}/app/js/nico-data-converter`);
-
-    global.Sortable = Sortable;
-    global.GridTable = GridTable;
-    global.RemoteDailog = RemoteDailog;
-    global.TimeFormat = TimeFormat;
-    global.NicoSearch = NicoSearch;
-    global.NicoVideoData = NicoVideoData;
-    global.NicoMylist = NicoMylist;
-    global.NicoUpdate = NicoUpdate;
-    global.History = History;
-    global.JsonStore = JsonStore;
-    global.BookMark = BookMark;
-    global.CacheStore = CacheStore;
-    global.VideoConverter = VideoConverter;
-    global.NicoDownloader = NicoDownloader;
-    global.GridTableDownloadItem = GridTableDownloadItem;
-    global.ScheduledTask = ScheduledTask;
-    global.ImportLibrary = ImportLibrary;
-    global.NicoDataConverter = NicoDataConverter;
+    global.Sortable = require("sortablejs");
+    global.GridTable = require(`${root_dir}/app/js/gridtable`);
+    global.RemoteDailog = require(`${root_dir}/app/js/remote-dialogs`);
+    global.TimeFormat = require(`${root_dir}/app/js/time-format`);
+    global.NicoSearch = require(`${root_dir}/app/js/nico-search`);
+    global.NicoVideoData = require(`${root_dir}/app/js/nico-data-file`);
+    global.NicoMylist = require(`${root_dir}/app/js/nico-mylist`);
+    global.NicoUpdate = require(`${root_dir}/app/js/nico-update`);
+    global.History = require(`${root_dir}/app/js/history`);
+    global.JsonStore = require(`${root_dir}/app/js/json-store`);
+    global.BookMark = require(`${root_dir}/app/js/bookmark`);
+    global.CacheStore = require(`${root_dir}/app/js/cache-store`);
+    global.VideoConverter = require(`${root_dir}/app/js/video-converter`);
+    global.NicoDownloader = require(`${root_dir}/app/js/nico-downloader`);
+    global.GridTableDownloadItem = require(`${root_dir}/app/js/gridtable-downloaditem`);
+    global.ScheduledTask = require(`${root_dir}/app/js/scheduled-task`);
+    global.ImportLibrary = require(`${root_dir}/app/js/import-library`);
+    global.NicoDataConverter = require(`${root_dir}/app/js/nico-data-converter`);
     global.ToastWrapper = require(`${root_dir}/app/js/toast-wrapper`);
 });
--- app/preload_player.js
+++ app/preload_player.js
@@ -1,21 +1,13 @@
-const electron = require("electron");
-const path = require("path");
-const EventEmitter = require("events");
-const Sortable = require("sortablejs");
-
-const root_dir = path.resolve(__dirname, "..");
-
-const IPC_CHANNEL = require(`${root_dir}/app/js/ipc-channel`);
-const logger = require(`${root_dir}/app/js/logger`);
+const root_dir = "..";
 
 process.once("loaded", () => {
     global.process = process;
-    global.electron = electron;
-    global.path = path;
-    global.module = module;
-    global.EventEmitter = EventEmitter;
-    global.IPC_CHANNEL = IPC_CHANNEL;
-    global.logger = logger;
+    global.electron = require("electron");
+    global.path = require("path");
+    // global.module = module;
+    global.EventEmitter = require("events");
+    global.IPC_CHANNEL = require(`${root_dir}/app/js/ipc-channel`);
+    global.logger = require(`${root_dir}/app/js/logger`);
 
     if (process.env.NODE_ENV == "DEBUG") {
         global.NicoMock = require(`${root_dir}/test/mock_server/nico-mock`);
@@ -25,7 +17,7 @@ process.once("loaded", () => {
 window.addEventListener( "error", async e => {
     const { message, filename, lineno, colno } = e;
     const msg = `${message}\n${filename}:${lineno}`;
-    logger.error(msg);
+    global.logger.error(msg);
     
     try {
         await global.RemoteDailog.showMessageBox("error", msg);
@@ -35,7 +27,7 @@ window.addEventListener( "error", async e => {
 } );
 
 window.addEventListener( "unhandledrejection", async e => {
-    logger.error(e.reason);
+    global.logger.error(e.reason);
    
     try {
         await global.RemoteDailog.showMessageBox("error", e.reason.message);
@@ -47,29 +39,17 @@ window.addEventListener( "unhandledrejection", async e => {
 window.addEventListener("load", () => {
     window.$ = window.jQuery = require("jquery");
     require("slickgrid/plugins/slick.autotooltips");
-    const GridTable = require(`${root_dir}/app/js/gridtable`);
-    const DataIpc = require(`${root_dir}/app/js/data-ipc`);
-    const RemoteDailog = require(`${root_dir}/app/js/remote-dialogs`);
-    const TimeFormat = require(`${root_dir}/app/js/time-format`);
-    const Niconico = require(`${root_dir}/app/js/niconico`);
-    const NicoPlay = require(`${root_dir}/app/js/nico-play`);
-    const NicoVideoData = require(`${root_dir}/app/js/nico-data-file`);
-    const CommentTimeLine = require(`${root_dir}/app/js/comment-timeline`);
-    const CommentFilter = require(`${root_dir}/app/js/comment-filter`);
-    const BookMark = require(`${root_dir}/app/js/bookmark`);
-    const SyncCommentScroll = require(`${root_dir}/app/js/sync-comment-scroll`);
-
-    global.Sortable = Sortable;
-    global.GridTable = GridTable;
-    global.DataIpc = DataIpc;
-    global.RemoteDailog = RemoteDailog;
-    global.TimeFormat = TimeFormat;
-    global.Niconico = Niconico;
-    global.NicoPlay = NicoPlay;
-    global.NicoVideoData = NicoVideoData;
-    global.CommentTimeLine = CommentTimeLine;
-    global.CommentFilter = CommentFilter;
-    global.BookMark = BookMark;
-    global.SyncCommentScroll = SyncCommentScroll;
+    // global.Sortable = require("sortablejs");
+    global.GridTable = require(`${root_dir}/app/js/gridtable`);
+    global.DataIpc = require(`${root_dir}/app/js/data-ipc`);
+    global.RemoteDailog = require(`${root_dir}/app/js/remote-dialogs`);
+    global.TimeFormat = require(`${root_dir}/app/js/time-format`);
+    global.Niconico = require(`${root_dir}/app/js/niconico`); //TODO
+    global.NicoPlay = require(`${root_dir}/app/js/nico-play`);
+    global.NicoVideoData = require(`${root_dir}/app/js/nico-data-file`);
+    global.CommentTimeLine = require(`${root_dir}/app/js/comment-timeline`);
+    global.CommentFilter = require(`${root_dir}/app/js/comment-filter`);
+    global.BookMark = require(`${root_dir}/app/js/bookmark`);
+    global.SyncCommentScroll = require(`${root_dir}/app/js/sync-comment-scroll`);
     global.ToastWrapper = require(`${root_dir}/app/js/toast-wrapper`);
 });

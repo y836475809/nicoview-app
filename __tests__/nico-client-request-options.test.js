97db0d7b3d0daaba5fb3fbd965c27dad819553e7
y836475809 <tttdaytttday@gmail.com>
2020-05-11 07:42:43 +0900

    「optionのときはjsonなし」に合わせてテスト修正

--- __tests__/nico-client-request-options.test.js
+++ __tests__/nico-client-request-options.test.js
@@ -8,7 +8,6 @@ const prof_time = new ProfTime();
 const test_host = "https://www.nock.test-options";
 const test_path = "/options/";
 const test_url = `${test_host}${test_path}`;
-const json_data = {key1:"data1"};
 
 const nock_options = (delay=1, code=200) => {
     nock(test_host)
@@ -43,7 +42,7 @@ test("options", async (t) => {
     nock_options();
 
     const req = new NicoClientRequest();
-    const body = await req.options(test_url, {json:json_data});
+    const body = await req.options(test_url);
     t.is(body, "ok");
 });
 
@@ -54,7 +53,7 @@ test("options timeout", async (t) => {
 
     try {
         const req = new NicoClientRequest();
-        await req.options(test_url, {json:json_data, timeout_msec:1*1000});
+        await req.options(test_url, {timeout_msec:1*1000});
     } catch (error) {
         t.is(error.cancel, undefined);
         t.is(error.name, "Error");
@@ -69,7 +68,7 @@ test("options page not find", async t => {
         
     try {
         const req = new NicoClientRequest();
-        await req.options(test_url, {json:json_data});
+        await req.options(test_url);
     } catch (error) {
         t.is(error.cancel, undefined);
         t.is(error.name, "Error");

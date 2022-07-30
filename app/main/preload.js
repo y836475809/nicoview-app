7af57e66f623d75b3dbc7a9b24ae4a78a485aef3
y836475809 <tttdaytttday@gmail.com>
2022-07-31 02:25:08 +0900

    change: preloadでの不要なrequireを削除

--- app/main/preload.js
+++ app/main/preload.js
@@ -1,11 +1,7 @@
-const root_dir = "../..";
 
 process.once("loaded", () => {
-    global.EventEmitter = require("events");
-    global.logger = require(`${root_dir}/app/js/logger`);
-    global.myapi = require(`${root_dir}/app/js/my-api`);
-    global.CommentTimeLine = require(`${root_dir}/app/js/comment-timeline`);
-    global.MyObservable = require(`${root_dir}/app/js/my-observable`);
+    global.logger = require("../../app/js/logger");
+    global.myapi = require("../../app/js/my-api");
 
     if(process.env["test_nicoappview"] ?? false){
         global.logger.logger.debug("test_nicoappview");
@@ -28,27 +24,6 @@ window.addEventListener( "unhandledrejection", async e => {
 } );
 
 window.addEventListener("load", () => {
-    global.Sortable = require("sortablejs");
-    global.GridTable = require(`${root_dir}/app/js/gridtable`);
-    global.TimeFormat = require(`${root_dir}/app/js/time-format`);
-    global.NicoSearch = require(`${root_dir}/app/js/nico-search`);
-    global.NicoVideoData = require(`${root_dir}/app/js/nico-data-file`);
-    global.NicoURL = require(`${root_dir}/app/js/nico-url`);
-    global.NicoMylist = require(`${root_dir}/app/js/nico-mylist`);
-    global.NicoUpdate = require(`${root_dir}/app/js/nico-update`);
-    global.CacheStore = require(`${root_dir}/app/js/cache-store`);
-    global.VideoConverter = require(`${root_dir}/app/js/video-converter`);
-    global.NicoDownloader = require(`${root_dir}/app/js/nico-downloader`);
-    global.GridTableDownloadItem = require(`${root_dir}/app/js/gridtable-downloaditem`);
-    global.ScheduledTask = require(`${root_dir}/app/js/scheduled-task`);
-    global.ImportFile = require(`${root_dir}/app/js/import-file`);
-    global.ImportNNDDSetting = require(`${root_dir}/app/js/import-nndd-setting`);
-    global.NicoDataConverter = require(`${root_dir}/app/js/nico-data-converter`);
-    global.Command = require(`${root_dir}/app/js/command`);
-    global.MouseGesture = require(`${root_dir}/app/js/mouse-gesture`);
-    global.ModalDialog = require(`${root_dir}/app/js/modal-dialog`);
-    global.ModalDialogUtil = require(`${root_dir}/app/js/modal-dialog-util`);
-    
     const params = new URLSearchParams(window.location.search);
     if(params.get("window") == "main"){
         global.RiotJS = {

dd9cb8646627515043f43b121db5010c6f0149a6
y836475809 <tttdaytttday@gmail.com>
2022-08-23 20:00:28 +0900

    fix:ブラウザコンパイルのタイミングによってエラーが出ていたのを修正

--- app/html/riot-mount.js
+++ app/html/riot-mount.js
@@ -8,8 +8,10 @@ const roitMount = (selector) => {  // eslint-disable-line no-unused-vars
     const wait_sec = 500;
     const int_id = setInterval(async () => { 
         // data-mountready=trueになっていれば準備OK
+        /** @type {HTMLElement} */
         const elm = document.querySelector("[data-mountready]");
         if(elm && elm.dataset.mountready=="true"){
+            elm.dataset.mountready = "done";
             await riot.compile();
             riot.mount(selector);
             clearInterval(int_id);
